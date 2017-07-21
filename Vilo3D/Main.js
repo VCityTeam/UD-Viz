3a965b754afedd798a5ed899c5729ef3b50768a5
Emmanuel Schmuck <emmanuel.schmuck@gmail.com>
2017-07-21 14:26:37 +0200

    added vilo3d and udv-code directories

---
+++ Vilo3D/Main.js
@@ -0,0 +1,109 @@
+
+// # Planar (EPSG:3946) viewer
+
+document.getElementById("info").innerHTML = "Vilo3D v0.1";
+
+
+// Define projection that we will use (taken from https://epsg.io/3946, Proj4js section)
+itowns.proj4.defs('EPSG:3946',
+    '+proj=lcc +lat_1=45.25 +lat_2=46.75 +lat_0=46 +lon_0=3 +x_0=1700000 +y_0=5200000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
+
+// Define geographic extent: CRS, min/max X, min/max Y
+const extent = new itowns.Extent(
+    'EPSG:3946',
+    1837816.94334, 1847692.32501,
+    5170036.4587, 5178412.82698);
+
+// ====================
+let renderer;
+// ====================
+
+// `viewerDiv` will contain iTowns' rendering area (`<canvas>`)
+const viewerDiv = document.getElementById('viewerDiv');
+// Instanciate PlanarView*
+var view = new itowns.PlanarView(viewerDiv, extent, { renderer });
+console.log(view);
+view.tileLayer.disableSkirt = true;
+
+// Add an WMS imagery layer (see WMS_Provider* for valid options)
+view.addLayer({
+    url: 'https://download.data.grandlyon.com/wms/grandlyon',
+    networkOptions: { crossOrigin: 'anonymous' },
+    type: 'color',
+    protocol: 'wms',
+    version: '1.3.0',
+    id: 'wms_imagery',
+    name: 'Ortho2009_vue_ensemble_16cm_CC46',
+    projection: 'EPSG:3946',
+    transparent: false,
+    extent,
+    bbox_url: 'wsen',
+    updateStrategy: {
+        type: 0,
+        options: {},
+    },
+    options: {
+        mimetype: 'image/jpeg',
+    },
+});
+
+// Add an WMS elevation layer (see WMS_Provider* for valid options)
+view.addLayer({
+    url: 'https://download.data.grandlyon.com/wms/grandlyon',
+    type: 'elevation',
+    protocol: 'wms',
+    networkOptions: { crossOrigin: 'anonymous' },
+    version: '1.3.0',
+    id: 'wms_elevation',
+    name: 'MNT2012_Altitude_10m_CC46',
+    style: '',
+    projection: 'EPSG:3946',
+    transparent: false,
+    extent,
+    bbox_url: 'wsen',
+    heightMapWidth: 256,
+    options: {
+        mimetype: 'image/jpeg',
+    },
+});
+// Since the elevation layer use color textures, specify min/max z
+view.tileLayer.materialOptions = {
+    useColorTextureElevation: true,
+    colorTextureElevationMinZ: 37,
+    colorTextureElevationMaxZ: 240,
+};
+
+//document.body.addEventListener("contextmenu", function(evt){evt.preventDefault();return false;});
+
+//camera control
+var control = new CameraControls(viewerDiv,view);
+
+
+
+// Position the camera at south-west corner
+//const c = new itowns.Coordinates('EPSG:3946', extent.west(), extent.south(), 2000);
+
+//var offset =new THREE.Vector3(3000,0,-7000);
+
+//offset = c.xyz();
+
+
+control.target = extent.center().xyz();
+
+control.update();
+
+//control.camera.position.add(offset);
+
+//control.update();
+
+//control.camera.lookAt(scope.target);
+
+console.log(control.camera.position);
+
+
+
+
+// Request redraw
+//view.notifyChange(true);
+
+//exports.view = view;
