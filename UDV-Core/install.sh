97b3011a2aa77713c24657ac26f957d70baba25d
sophiaab <sophia.aboujouda@cpe.fr>
2018-05-28 10:29:03 +0200

    modify install

--- UDV-Core/install.sh
+++ UDV-Core/install.sh
@@ -6,14 +6,14 @@ cd dist
 #git clone https://github.com/sophiaab/itowns.git
 #git clone https://github.com/jailln/itowns.git
 
-# git clone https://github.com/itowns/itowns.git
-# cd itowns
-# git checkout example_fix_747
-# npm install
-# npm pack
-# cd ..
-# cd ..
-# npm install dist/itowns/itowns-2.2.0.tgz
+git clone https://github.com/itowns/itowns.git
+cd itowns
+git checkout example_fix_747
+npm install
+npm pack
+cd ..
+cd ..
+npm install dist/itowns/itowns-2.2.0.tgz
 
 npm install
 ### The following is only needed in server mode (since in devel mode
