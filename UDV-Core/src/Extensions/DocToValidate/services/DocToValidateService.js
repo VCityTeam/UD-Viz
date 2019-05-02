2f8e219d119aa62addf72c4af0ab130bbc66ba1a
Valentin Rigolle <valentin.rigolle@gmail.com>
2019-05-02 14:47:53 +0200

    Filtre fonctionnel

--- UDV-Core/src/Extensions/DocToValidate/services/DocToValidateService.js
+++ UDV-Core/src/Extensions/DocToValidate/services/DocToValidateService.js
@@ -3,24 +3,21 @@ export function DocToValidateService(requestService, config) {
     this.requestService = requestService;
 
     this.documents = [];
+    this.sampleDocuments = []; 
     this.currentDocumentId = 0;
 
     this.observers = [];
 
     this.initialize = function () {
         console.log('Doc To Validate Service initialized.');
-    }
-
-    this.search = function (filterFormData) {
-        //request to fetch docs
-        this.documents = [
+        this.sampleDocuments = [
             {
                 id: '1',
                 title: 'Title 1',
                 subject: 'Subject 1',
                 description: 'Description 1',
                 type: 'type 1',
-                referringDate: '1930-01-01',
+                referringDate: '2017-01-01',
                 publicationDate: '2018-07-11',
                 imgUrl: '../services/pikachu.png'
             },
@@ -30,12 +27,34 @@ export function DocToValidateService(requestService, config) {
                 subject: 'Subject 2',
                 description: 'Description 2',
                 type: 'Type 2',
-                referringDate: '1930-01-01',
-                publicationDate: '2018-07-11',
+                referringDate: '2020-01-01',
+                publicationDate: '2021-07-11',
                 imgUrl: '../services/sonic.jpg'
             }
         ];
-        this.currentDocumentId = 0;
+    }
+
+    this.search = function (filterFormData) {
+        //request to fetch docs
+        this.documents = this.sampleDocuments;
+
+        //Code by mazine
+        const keywordFilter = filterFormData.get("keyword"); 
+        const startReferringDateFilter = filterFormData.get("startReferringDate");
+        const endReferringDateFilter = filterFormData.get("endReferringDate");
+        const startPublicationDateFilter = filterFormData.get("startPublicationDate");
+        const endPublicationDateFilter = filterFormData.get("endPublicationDate");
+        
+        const result = this.documents.filter(document => (keywordFilter === undefined || keywordFilter === null || keywordFilter === '' ||document.title.includes(keywordFilter)) &&
+        (startReferringDateFilter === undefined || startReferringDateFilter === null || startReferringDateFilter === '' || document.referringDate > startReferringDateFilter) && 
+        (endReferringDateFilter === undefined || endReferringDateFilter === null || endReferringDateFilter === '' || document.referringDate < endReferringDateFilter) &&
+        (startPublicationDateFilter === undefined || startPublicationDateFilter === null ||startPublicationDateFilter === '' ||  document.publicationDate > startPublicationDateFilter) && 
+        (endPublicationDateFilter === undefined || endPublicationDateFilter === null || endPublicationDateFilter === '' || document.publicationDate < endPublicationDateFilter) 
+        );
+
+        this.documents = result;
+        this.currentDocumentId = 0; 
+
     }
 
     this.clearSearch = function () {
