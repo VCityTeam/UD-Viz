8bec91127e4c400094fc3b17ad65bd0ec8942493
Valentin Rigolle <valentin.rigolle@gmail.com>
2019-05-07 15:30:59 +0200

    Amélioré fenêtre commentaire

--- UDV-Core/src/Extensions/DocToValidate/views/DocToValidateCommentWindow.js
+++ UDV-Core/src/Extensions/DocToValidate/views/DocToValidateCommentWindow.js
@@ -23,45 +23,39 @@ export class DocToValidateCommentWindow extends Window {
         `;
     }
 
-    getComment()
-    {
-      this.docToValidateService.getComments().then((comments) => {
-        document.getElementById('docToValidateComment_left').innerHTML = '';
-        console.log(comments);
-        for(let comment of comments)
-        {
-            let div = document.createElement('div');
-            div.className = 'talk-bubble';
-            div.innerHTML = `
+    getComment() {
+        this.docToValidateService.getComments().then((comments) => {
+            document.getElementById('docToValidateComment_left').innerHTML = '';
+            console.log(comments);
+            for (let comment of comments.reverse()) {
+                let div = document.createElement('div');
+                div.className = 'talk-bubble';
+                div.innerHTML = `
             <div class="talktext">
             </div>
             <p> ${comment.description} </p>
             `;
-            document.getElementById('docToValidateComment_left').appendChild(div);
-        }
-      });
+                document.getElementById('docToValidateComment_left').appendChild(div);
+            }
+        });
     }
-
 
     windowCreated() {
-      this.window.style.setProperty('min-width', '400px');
-      this.window.style.setProperty('min-height', '300px');
-      document.getElementById('docToValidateComment_inputButton').onclick = this.publishComment.bind(this);
-      this.getComment();
-
-
-
+        this.window.style.setProperty('min-width', '450px');
+        this.window.style.setProperty('min-height', '300px');
+        this.window.style.setProperty('left', '1100px');
+        this.window.style.setProperty('resize', 'none');
+        document.getElementById('docToValidateComment_inputButton').onclick = this.publishComment.bind(this);
+        this.getComment();
     }
 
-
-
     publishComment() {
-      console.log('enter')
-      let form = document.getElementById('docToValidateComment_inputForm');
-      let form_data = new FormData(form);
-      console.log(form_data);
-      this.docToValidateService.publishComment(form_data).then(()=>{
-          this.getComment();
-      }) ;
+        console.log('enter')
+        let form = document.getElementById('docToValidateComment_inputForm');
+        let form_data = new FormData(form);
+        console.log(form_data);
+        this.docToValidateService.publishComment(form_data).then(() => {
+            this.getComment();
+        });
     }
 }
