c85e5b6d9f2006ddb213927fd5000c473ac46d7c
Valentin Rigolle <valentin.rigolle@gmail.com>
2019-05-14 17:30:25 +0200

    Renamed folder + updated Help

--- UDV-Core/src/Modules/Others/Help.js
+++ UDV-Core/src/Modules/Others/Help.js
@@ -1,72 +1,54 @@
 import './Help.css';
+import { ModuleView } from '../../Utils/ModuleView/ModuleView';
 
 /**
 * adds a "Help" window that can be open/closed with a button
 * simply include this file in the html, no need to instanciate anything in main.js
 */
 
-export function HelpWindow(options = {}) {
+export class HelpWindow extends ModuleView {
 
-  ///////////// Html elements
-  var helpDiv = document.createElement("div");
-  helpDiv.id = 'helpWindow';
-  $("#contentSection").append(helpDiv);
+  constructor(options = {}) {
+    super();
 
-  document.getElementById("helpWindow").innerHTML =
-    '<div id="text">\
-       <br>\
-       <p><a target="_blank"\
-       href="https://github.com/iTowns/itowns/blob/master/src/Renderer/ThreeExtended/PlanarControls.js">Camera key bindings</a>:</p>\
-       <ul>\
-         <li>Left-Click: camera translation (drag)</li>\
-         <li>Right-Click: camera translation (pan)</li>\
-         <li>Ctrl + Left-Click: camera rotation (orbit)</li>\
-         <li>Spacebar / Wheel-Click: smart zoom</li>\
-         <li>Mouse Wheel: zoom in/out</li>\
-         <li>T: orient camera to a top view</li>\
-         <li>Y: move camera to start position</li>\
-       </ul>\
-       <button id="helpCloseButton">Close</button>\
-    </div>\
-    ';
+    ///////////// Html elements
+    var helpDiv = document.createElement("div");
+    helpDiv.id = 'helpWindow';
+    $("#contentSection").append(helpDiv);
 
-  // Close the window...when close button is hit
-  document.getElementById("helpCloseButton").addEventListener(
-    'mousedown', () => {
-      this.disable();
-    }, false);
+    document.getElementById("helpWindow").innerHTML =
+      '<div id="text">\
+         <br>\
+         <p><a target="_blank"\
+         href="https://github.com/iTowns/itowns/blob/master/src/Renderer/ThreeExtended/PlanarControls.js">Camera key bindings</a>:</p>\
+         <ul>\
+           <li>Left-Click: camera translation (drag)</li>\
+           <li>Right-Click: camera translation (pan)</li>\
+           <li>Ctrl + Left-Click: camera rotation (orbit)</li>\
+           <li>Spacebar / Wheel-Click: smart zoom</li>\
+           <li>Mouse Wheel: zoom in/out</li>\
+           <li>T: orient camera to a top view</li>\
+           <li>Y: move camera to start position</li>\
+         </ul>\
+         <button id="helpCloseButton">Close</button>\
+      </div>\
+      ';
+
+    // Close the window...when close button is hit
+    document.getElementById("helpCloseButton").addEventListener(
+      'mousedown', () => {
+        this.disable();
+      }, false);
+  }
 
 
-  /////// MODULE MANAGEMENT FOR BASE DEMO
+  /////// MODULE VIEW METHODS
 
-  this.enable = () => {
+  enableView() {
     document.getElementById('helpWindow').style.setProperty('display', 'block');
-    this.sendEvent('ENABLED');
   }
 
-  this.disable = () => {
+  disableView() {
     document.getElementById('helpWindow').style.setProperty('display', 'none');
-    this.sendEvent('DISABLED');
-  }
-
-  this.eventListeners = {};
-
-  this.addEventListener = (event, action) => {
-    if (this.eventListeners[event]) {
-      this.eventListeners[event].push(action);
-    } else {
-      this.eventListeners[event] = [
-        action
-      ];
-    }
-  }
-
-  this.sendEvent = (event) => {
-    let listeners = this.eventListeners[event];
-    if (listeners !== undefined && listeners !== null) {
-        for (let listener of listeners) {
-            listener();
-        }
-    }
   }
 }
