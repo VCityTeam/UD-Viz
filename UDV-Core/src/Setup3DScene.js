de4d369ceb0eab1dbe8f5fd53b26a2a48d7c4eee
Valentin Rigolle <valentin.rigolle@gmail.com>
2019-05-14 11:26:59 +0200

    Starting to make a base demo class (BaseDemo) + adapting 'About' Module

--- UDV-Core/src/Setup3DScene.js
+++ UDV-Core/src/Setup3DScene.js
@@ -17,7 +17,8 @@ import proj4 from 'proj4';
 //=============================================================================
 export function Setup3DScene(terrainAndElevationRequest,
                              buildingServerRequest,
-                             showBuildings = false )
+                             showBuildings = false,
+                             view)
 {
 // Define projection that we will use (taken from https://epsg.io/3946, Proj4js section)
 proj4.defs('EPSG:3946',
