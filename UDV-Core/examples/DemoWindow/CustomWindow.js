fff18f79b4de2ad7e964f0cb27a1143ed04c88de
Valentin Rigolle <valentin.rigolle@gmail.com>
2019-07-22 12:28:11 +0200

    Revert custom window

--- UDV-Core/examples/DemoWindow/CustomWindow.js
+++ UDV-Core/examples/DemoWindow/CustomWindow.js
@@ -8,17 +8,15 @@ export class CustomWindow extends Window {
         this.windowDisplayWhenVisible = 'block';
     }
 
-    get html () {
+    get innerContentHtml () {
         return `
-            <p style="background: red; border: 1px solid black; width: 300px; height: 300px;">
+            <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
         `;
     }
 
     windowCreated () {
-        console.log(this.window);
-        this.window.className = "";
         console.log('created');
     }
 
