967f0bc34ebb5204dd0071e7c9b897c43b6c727c
jailln <vincentjaillot@gmail.com>
2019-11-05 15:01:37 +0100

    Config changes

--- UDV-Core/examples/DemoArticle/Demo.js
+++ UDV-Core/examples/DemoArticle/Demo.js
@@ -10,8 +10,8 @@ baseDemo.appendTo(document.body);
 baseDemo.loadConfigFile('../data/config/generalDemoConfig.json').then(() => {
     // Initialize iTowns 3D view
     baseDemo.init3DView('lyon_all_districts');
-    baseDemo.add3DTilesLayer('building-lyon-2009');
+    // baseDemo.add3DTilesLayer('building-lyon-2009');
     // baseDemo.add3DTilesLayer('building-lyon-2012');
-    // baseDemo.add3DTilesLayer('building-lyon-2015');
+    baseDemo.add3DTilesLayer('building-lyon-2015');
     baseDemo.update3DView();
 });
