52150a1438a7c2acc8691959d2378636f79212e0
lamache <val@portable>
2021-03-11 14:35:16 +0100

    clean dev.js

--- bin/dev.js
+++ bin/dev.js
@@ -1,7 +1,6 @@
 /** @format */
 
 const exec = require('child-process-promise').exec;
-const { spawn } = require('child_process');
 
 const printExec = function (result) {
   console.log('stdout: \n', result.stdout);
