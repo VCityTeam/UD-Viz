a741fbc43f07a62f1da083a1434bdf3b5d3e48ee
valentinMachado <valentin.machado.cpe@gmail.com>
2021-06-04 14:19:05 +0200

    transform class

--- src/Game/Shared/Components/THREEUtils.js
+++ src/Game/Shared/Components/THREEUtils.js
@@ -38,9 +38,65 @@ module.exports = {
     // renderer.toneMappingExposure = 1;
   },
 
-  Transform: class {
-    constructor() {
-      console.log('new transform');
+  Transform: class Transform {
+    constructor(position, rotation, scale) {
+      this.position = position || new THREE.Vector3();
+      this.rotation = rotation || new THREE.Vector3();
+      this.scale = scale || new THREE.Vector3(1, 1, 1);
+    }
+
+    getPosition() {
+      return this.position;
+    }
+
+    setPosition(position) {
+      this.position = position;
+    }
+
+    getRotation() {
+      return this.rotation;
+    }
+
+    setRotation(rotation) {
+      this.rotation = rotation;
+    }
+
+    getScale() {
+      return this.scale;
+    }
+
+    setScale(scale) {
+      this.scale = scale;
+    }
+
+    clone() {
+      return new Transform(
+        this.position.clone(),
+        this.rotation.clone(),
+        this.scale.clone()
+      );
+    }
+
+    setFromJSON(json) {
+      if (json) {
+        if (json.position) {
+          this.position.x = json.position.x;
+          this.position.y = json.position.y;
+          this.position.z = json.position.z;
+        }
+
+        if (json.rotation) {
+          this.rotation.x = json.rotation.x;
+          this.rotation.y = json.rotation.y;
+          this.rotation.z = json.rotation.z;
+        }
+
+        if (json.scale) {
+          this.scale.x = json.scale.x;
+          this.scale.y = json.scale.y;
+          this.scale.z = json.scale.z;
+        }
+      }
     }
   },
 };
