80d66b8b279d3d87a51867fbb7aac03cd3f2c1f9
valentinMachado <valentin.machado.cpe@gmail.com>
2021-06-04 12:25:29 +0200

    refacto bis

--- src/Game/Shared/Components/Components.js
+++ src/Game/Shared/Components/Components.js
@@ -2,8 +2,10 @@
 
 const commonJsData = require('./Data');
 const commonJsPrefabUtils = require('./PrefabUtils');
+const commonJsTHREEUtils = require('./THREEUtils');
 
 module.exports = {
   Data: commonJsData,
   PrefabUtils: commonJsPrefabUtils,
+  THREEUtils: commonJsTHREEUtils,
 };
