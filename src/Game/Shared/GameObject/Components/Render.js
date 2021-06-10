8bdd3a84c809aa276bc2f226bd0616599d42dd52
valentinMachado <valentin.machado.cpe@gmail.com>
2021-06-10 16:44:38 +0200

    remove name of Render + cleans

--- src/Game/Shared/GameObject/Components/Render.js
+++ src/Game/Shared/GameObject/Components/Render.js
@@ -7,8 +7,6 @@ const RenderModule = class Render {
     this.parent = parent;
     this.uuid = json.uuid || THREE.MathUtils.generateUUID();
     this.idModel = json.idModel || null;
-    this.media = json.media || null;
-    this.name = json.name || null; //TODO display it above object3D
 
     this.color = new THREE.Color().fromArray(json.color || [1, 1, 1]);
 
@@ -24,8 +22,6 @@ const RenderModule = class Render {
   toJSON() {
     return {
       uuid: this.uuid,
-      name: this.name,
-      media: this.media,
       type: RenderModule.TYPE,
       idModel: this.idModel,
       color: this.color.toArray(),
@@ -44,37 +40,20 @@ const RenderModule = class Render {
     return this.originalObject3D;
   }
 
-  setName(value, assetsManager) {
-    const oldParent = this.object3D.parent;
-    oldParent.remove(this.object3D);
-    this.name = value;
-    this.initAssets(assetsManager);
-    oldParent.add(this.parent.fetchObject3D());
-  }
-
-  getName() {
-    return this.name;
-  }
-
-  setColor(value, assetsManager) {
-    const oldParent = this.object3D.parent;
-    oldParent.remove(this.object3D);
+  setColor(value) {
     this.color = value;
-    this.initAssets(assetsManager);
-    oldParent.add(this.parent.fetchObject3D());
+    this.object3D.traverse(function (c) {
+      if (c.material) c.material.color = value;
+    });
   }
 
   getColor() {
     return this.color;
   }
 
-  updateFromComponent(component, assetsManager) {
-    if (this.name != component.getName()) {
-      this.setName(component.getName(), assetsManager);
-    }
-
+  updateFromComponent(component) {
     if (!this.color.equals(component.getColor())) {
-      this.setColor(component.getColor(), assetsManager);
+      this.setColor(component.getColor());
     }
   }
 
@@ -97,33 +76,7 @@ const RenderModule = class Render {
       this.object3D.add(assetsManager.createModel(this.idModel));
     }
 
-    //TODO remove media and name and handle with a local script
-
-    //name display above model
-    if (this.name) {
-      const bb = this.computeBoundingBox();
-      const sprite = assetsManager.createSprite(this.name);
-      const bbSprite = new THREE.Box3().setFromObject(sprite);
-      sprite.position.z = bb.max.z + 0.5 * (bbSprite.max.y - bbSprite.min.y);
-      this.object3D.add(sprite);
-    }
-
-    if (this.media) {
-
-      if (this.media.img) {
-        const mediaImg = assetsManager.createImage(
-          this.media.img.path,
-          this.media.img.width,
-          this.media.img.height
-        );
-        this.object3D.add(mediaImg);
-      }
-    }
-
-    const color = this.color;
-    this.object3D.traverse(function (c) {
-      if (c.material) c.material.color = color;
-    });
+    this.setColor(this.color);
 
     this.originalObject3D = this.object3D.clone(); //keep a copy of it
 
@@ -132,14 +85,6 @@ const RenderModule = class Render {
 };
 
 RenderModule.TYPE = 'Render';
-
-RenderModule.bindName = function (goJSON, name) {
-  try {
-    goJSON.components.Render.name = name;
-  } catch (e) {
-    throw new Error(e);
-  }
-};
 
 RenderModule.bindColor = function (goJSON, color) {
   try {
