df178264eb0a05686c11dcff78d4d9772fe625a1
valentinMachado <valentin.machado.cpe@gmail.com>
2021-06-21 14:18:27 +0200

    SystemUtils doc

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -2,7 +2,6 @@
 
 export { RequestService } from './Request/RequestService';
 
-//this kind of component should be at this level of API
 import * as SystemUtils from './SystemUtils/SystemUtils';
 export { SystemUtils };
 
