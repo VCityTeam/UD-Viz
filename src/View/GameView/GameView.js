b87ab0473c7d3588ee01898916f20629b964ae3d
valentinMachado <valentin.machado.cpe@gmail.com>
2021-06-21 12:11:51 +0200

    Gameview doc

--- src/View/GameView/GameView.js
+++ src/View/GameView/GameView.js
@@ -10,6 +10,10 @@ import { View3D } from '../View3D/View3D';
 const udvShared = require('../../Game/Shared/Shared');
 const THREEUtils = udvShared.Components.THREEUtils;
 
+/**
+ * Main view of an ud-viz game application
+ * This object works with a state computer (./src/Game/Components/StateComputer)
+ */
 export class GameView extends View3D {
   constructor(params) {
     //call parent class
@@ -18,58 +22,71 @@ export class GameView extends View3D {
     //assets
     this.assetsManager = params.assetsManager;
 
-    //state renderer
+    //state computer
     this.stateComputer = params.stateComputer;
 
-    //object
+    //object3D
     this.object3D = new THREE.Object3D();
     this.object3D.name = 'GameView_Object3D';
 
     //sky color
     this.skyColor = null;
 
-    //register last pass
+    //the last state processed
     this.lastState = null;
 
-    //first game view to know if traveling
-    this.firstGameView = params.firstGameView || false;
+    //TODO place these attributes in a userData object
+    this.firstGameView = params.firstGameView || false; //first gameview of the application
+    this.avatarUUID = null; //uuid of the avatar
 
-    //uuid avatar TODO remove
-    this.avatarUUID = null;
-
-    //to pass local script
+    //context pass to the localScript GameObject
     this.localContext = new LocalContext(this);
 
-    //ref uuid of go in the last state
+    //Current GameObject UUID in the last state
     this.currentUUID = {};
 
-    //move in View3D ?
+    //TODO move in View3D
+    //Array of callbacks call during the tick
     this.tickRequesters = [];
   }
 
+  /**
+   *
+   * @returns {THREE.Color} color of the renderer clear color
+   */
   getSkyColor() {
     return this.skyColor;
   }
 
+  /**
+   * register the function into tickRequesters
+   * @param {Function} cb a function that will be call every tick
+   */
   addTickRequester(cb) {
     this.tickRequesters.push(cb);
   }
 
+  /**
+   * Initialize this view
+   *
+   * @param {WorldState} state first state of this view
+   * @param {uuid} avatarUUID uuid of the avatar GameObject
+   */
   onFirstState(state, avatarUUID) {
+    //ref it
     this.avatarUUID = avatarUUID;
 
     //build itowns view
     const o = state.getOrigin();
     const [x, y] = proj4.default('EPSG:3946').forward([o.lng, o.lat]);
     const r = this.config.itowns.radiusExtent;
-
     // Define geographic extent: CRS, min/max X, min/max Y
     const extent = new itowns.Extent('EPSG:3946', x - r, x + r, y - r, y + r);
-
     this.initItownsView(extent);
+
     this.initScene(state);
 
-    //register in mainloop
+    //start to tick
     const _this = this;
     const fps = this.config.game.fps;
 
@@ -77,7 +94,7 @@ export class GameView extends View3D {
     let then = Date.now();
     let delta;
     const tick = function () {
-      if (_this.disposed) return; //stop requesting frame
+      if (_this.disposed) return; //stop requesting frame if disposed
 
       requestAnimationFrame(tick);
 
@@ -102,35 +119,41 @@ export class GameView extends View3D {
     };
     tick();
 
-    //resize
+    //differed a resize event
     setTimeout(this.onResize.bind(this), 1000);
   }
 
+  /**
+   * initialize the scene of the itwons view
+   * @param {WorldState} state
+   */
   initScene(state) {
     const o = state.getOrigin();
     const [x, y] = proj4.default('EPSG:3946').forward([o.lng, o.lat]);
 
+    //add the object3D of the Game
     this.object3D.position.x = x;
     this.object3D.position.y = y;
     this.object3D.position.z = o.alt;
     this.itownsView.scene.add(this.object3D);
 
-    //sky
+    //init sky color based on config file
     this.skyColor = new THREE.Color(
       this.config.game.skyColor.r,
       this.config.game.skyColor.g,
       this.config.game.skyColor.b
     );
 
-    //shadow
+    //init renderer
     const renderer = this.itownsView.mainLoop.gfxEngine.renderer;
     THREEUtils.initRenderer(renderer, this.skyColor);
 
-    // Lights
+    //add lights
     const { directionalLight, ambientLight } = THREEUtils.addLights(
       this.itownsView.scene
     );
 
+    //configure shadows based on a config files
     directionalLight.shadow.mapSize = new THREE.Vector2(
       this.config.game.shadowMapSize,
       this.config.game.shadowMapSize
@@ -140,19 +163,28 @@ export class GameView extends View3D {
     this.directionalLight = directionalLight;
   }
 
+  /**
+   * Update GameObject with the new state
+   * Initialize assets of the new GameObject
+   * Call LocalScript of the GameObject
+   * Replace Shadow light if needed
+   * Call a render pass
+   *
+   * @param {WorldState} state the new state used to update this view
+   */
   update(state) {
     const _this = this;
     const newGO = [];
     const ctx = this.localContext;
 
+    //update lastState with the new one
     if (this.lastState) {
-      if (!state.getGameObject()) throw new Error('no gameObject in state');
-
       let lastGO = this.lastState.getGameObject();
       lastGO.traverse(function (g) {
         const uuid = g.getUUID();
         const current = state.getGameObject().find(uuid);
         if (current && !g.isStatic()) {
+          //update local components
           g.updateNoStaticFromGO(current, ctx);
         } else if (!current) {
           //do not exist remove it
@@ -182,15 +214,15 @@ export class GameView extends View3D {
       });
     }
 
-    //buffer
+    //bufferize
     this.lastState = state;
 
-    const go = state.getGameObject();
-
-    //init new GO
+    //init assets new GO
     newGO.forEach(function (g) {
       g.initAssetsComponents(_this.assetsManager, udvShared);
     });
+
+    const go = state.getGameObject();
 
     //localscript event INIT + ON_NEW_GAMEOBJECT
     newGO.forEach(function (g) {
@@ -203,7 +235,7 @@ export class GameView extends View3D {
         scriptComponent.execute(LocalScript.EVENT.INIT, [ctx]);
       }
 
-      //notify other go that
+      //notify other go that a new go has been added
       go.traverse(function (child) {
         const scriptComponent = child.getComponent(LocalScript.TYPE);
         if (scriptComponent) {
@@ -230,52 +262,85 @@ export class GameView extends View3D {
         this.directionalLight
       );
 
-    if (this.pause) return; //no render
+    if (!this.pause) {
+      //tick local script
+      go.traverse(function (child) {
+        const scriptComponent = child.getComponent(LocalScript.TYPE);
+        if (scriptComponent)
+          scriptComponent.execute(LocalScript.EVENT.TICK, [ctx]);
+      });
 
-    //tick local script
-    go.traverse(function (child) {
-      const scriptComponent = child.getComponent(LocalScript.TYPE);
-      if (scriptComponent)
-        scriptComponent.execute(LocalScript.EVENT.TICK, [ctx]);
-    });
+      //render
+      const scene = this.itownsView.scene;
+      const renderer = this.itownsView.mainLoop.gfxEngine.renderer;
+      renderer.clearColor();
+      renderer.render(scene, this.itownsView.camera.camera3D);
 
-    //render
-    const scene = this.itownsView.scene;
-    const renderer = this.itownsView.mainLoop.gfxEngine.renderer;
-    renderer.clearColor();
-    renderer.render(scene, this.itownsView.camera.camera3D);
-
-    //TODO refacto tick integrate with itowns rendering
+      //TODO refacto tick integrate with itowns rendering
+    }
   }
 
+  /**
+   * @returns {AssetsManager}
+   */
   getAssetsManager() {
     return this.assetsManager;
   }
 
+  /**
+   * @returns {WorldState}
+   */
   getLastState() {
     return this.lastState;
   }
 }
 
+/**
+ * Context pass to the GameObject LocalScript to work
+ * TODO pass ud-viz module instead of just Shared/itowns/proj4
+ */
 class LocalContext {
   constructor(gameView) {
     this.dt = 0;
     this.gameView = gameView;
   }
 
+  /**
+   *
+   * @param {Number} dt delta time of the current frame
+   */
   setDt(dt) {
     this.dt = dt;
   }
 
+  /**
+   * ud-viz/Game/Shared module
+   * @returns {Shared}
+   */
   getSharedModule() {
     return udvShared;
   }
+
+  /**
+   *
+   * @returns {Number}
+   */
   getDt() {
     return this.dt;
   }
+
+  /**
+   *
+   * @returns {GameView}
+   */
   getGameView() {
     return this.gameView;
   }
+
+  /**
+   * itowns module
+   * @returns {itowns}
+   */
   getItownsModule() {
     return itowns;
   }
