38428c7ed7420d5a4b6e27c89b55a27a66be61d0
valentinMachado <valentin.machado.cpe@gmail.com>
2021-06-21 15:21:33 +0200

    localcomputer doc

--- src/Game/Components/StateComputer/LocalComputer.js
+++ src/Game/Components/StateComputer/LocalComputer.js
@@ -5,6 +5,9 @@ const Shared = require('../../Shared/Shared');
 
 //TODO LocalComputer do not have tick to make tick the world
 
+/**
+ * StateComputer working locally
+ */
 export class LocalComputer {
   constructor(world, assetsManager) {
     this.worldContext = new WorldContext({
@@ -14,12 +17,21 @@ export class LocalComputer {
     });
   }
 
+  /**
+   * Load world
+   * @param {Function} onLoad callback called when world is loaded
+   */
   load(onLoad) {
     const _this = this;
     this.worldContext.getWorld().load(onLoad, this.worldContext);
   }
 
-  //API call from GameView
+  //StateComputer INTERFACE
+
+  /**
+   * Compute the current world state
+   * @returns {WorldState}
+   */
   computeCurrentState() {
     return this.worldContext.getWorld().computeWorldState();
   }
