185c952b805b3564cfe9248d9dc921cc9a815e46
valentinMachado <valentin.machado.cpe@gmail.com>
2021-06-22 11:12:55 +0200

    routine doc

--- src/Game/Shared/Components/Routine.js
+++ src/Game/Shared/Components/Routine.js
@@ -1,8 +1,19 @@
 /** @format */
 
+/**
+ * Object used to handle animation
+ * TODO refacto this class so user of this dont need to handle tick + onEnd call
+ */
 module.exports = class Routine {
   constructor(tick, onEnd) {
+    /**
+     * @type {Function} when this function return true means the animation is finished
+     */
     this.tick = tick;
+
+    /**
+     * @type {Function} callback called at the end of the animation
+     */
     this.onEnd = onEnd;
   }
 };
