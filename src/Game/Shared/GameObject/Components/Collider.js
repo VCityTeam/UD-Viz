1432e4f2c31b776aca8fd43930a655198b38cc8c
valentinMachado <valentin.machado.cpe@gmail.com>
2021-06-22 15:41:53 +0200

    Collider doc

--- src/Game/Shared/GameObject/Components/Collider.js
+++ src/Game/Shared/GameObject/Components/Collider.js
@@ -3,28 +3,50 @@
 const THREE = require('three');
 const { Circle, Polygon } = require('detect-collisions');
 
+/**
+ * Component used to handle collision of a GameObject
+ * Support by detect-collisions npm package
+ */
 const ColliderModule = class Collider {
   constructor(parent, json) {
     if (!json) throw new Error('no json');
+
+    //gameobject of this component
     this.parent = parent;
+
+    //uuid
     this.uuid = json.uuid || THREE.MathUtils.generateUUID();
+
+    //shapes in json format
     this.shapesJSON = json.shapes || [];
 
+    //boolean to know if its a physics collisions or not
     this.body = json.body || false;
 
-    //data
+    //shapes wrappers
     this.shapeWrappers = [];
     this.createShapeWrappers();
   }
 
+  /**
+   *
+   * @returns {Boolean}
+   */
   isBody() {
     return this.body;
   }
 
+  /**
+   * Nothing has to be initialized (just here because this function is called from GameObject initAssets)
+   * @param {*} assetsManager
+   */
   initAssets(assetsManager) {
     //nada
   }
 
+  /**
+   * Create ShapeWrappers object from the json shapes
+   */
   createShapeWrappers() {
     const shapeWrappers = this.shapeWrappers;
     const _this = this;
@@ -34,10 +56,17 @@ const ColliderModule = class Collider {
     });
   }
 
+  /**
+   *
+   * @returns {Array[ShapeWrapper]}
+   */
   getShapeWrappers() {
     return this.shapeWrappers;
   }
 
+  /**
+   * Update worldtransform of the shapeWrappers
+   */
   update() {
     const worldTransform = this.parent.computeWorldTransform();
     this.shapeWrappers.forEach(function (b) {
@@ -45,18 +74,34 @@ const ColliderModule = class Collider {
     });
   }
 
+  /**
+   * This component can run on the server side
+   * @returns {Boolean}
+   */
   isServerSide() {
     return true;
   }
 
+  /**
+   *
+   * @param {JSON} json
+   */
   setShapesJSON(json) {
     this.shapesJSON = json;
   }
 
+  /**
+   *
+   * @returns {JSON}
+   */
   getShapesJSON() {
     return this.shapesJSON;
   }
 
+  /**
+   * Compute this to JSON
+   * @returns {JSON}
+   */
   toJSON() {
     return {
       uuid: this.uuid,
@@ -71,25 +116,46 @@ ColliderModule.TYPE = 'Collider';
 
 module.exports = ColliderModule;
 
+/**
+ * Object to wrap the Polygon and Circle of the detect-collisions npm package
+ */
 class ShapeWrapper {
   constructor(gameObject, json) {
+    //gameobject of this shapewrapper
     this.gameObject = gameObject;
+
+    //json
     this.json = json;
 
-    //sgape detect - collision
+    //shape detect-collisions npm package
     this.shape = null;
 
+    //init
     this.initFromJSON(json);
   }
 
+  /**
+   *
+   * @returns {Circle/Polygon}
+   */
   getShape() {
     return this.shape;
   }
 
+  /**
+   *
+   * @returns {GameObject}
+   */
   getGameObject() {
     return this.gameObject;
   }
 
+  /**
+   * Create Circle/Polygon of detect-collisions
+   * then add an update function to update the worldtransform
+   * then attach getter function of the gameobject
+   * @param {JSON} json
+   */
   initFromJSON(json) {
     switch (json.type) {
       case 'Circle':
