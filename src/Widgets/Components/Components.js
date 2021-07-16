7cc3aa9c464b6e3b8c9932c2c8fa02e5c2fec84c
Diego Vinasco <diego.vinasco@protonmail.com>
2021-07-16 18:05:38 +0200

    added Window export to Widgets.Components

--- src/Widgets/Components/Components.js
+++ src/Widgets/Components/Components.js
@@ -5,3 +5,5 @@ export { ModuleView } from './ModuleView/ModuleView.js';
 export { TilesManager } from './3DTiles/TilesManager.js';
 
 export { LayerManager } from './LayerManager/LayerManager.js';
+
+export { Window } from './GUI/js/Window.js';
