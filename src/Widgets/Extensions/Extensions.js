f19dce8cf5fff5e93783bae43af08d66c3fa26a6
valentinMachado <valentin.machado.cpe@gmail.com>
2021-09-16 10:51:24 +0200

    move help widget to extensions

--- src/Widgets/Extensions/Extensions.js
+++ src/Widgets/Extensions/Extensions.js
@@ -15,3 +15,5 @@ export { Debug3DTilesWindow } from './3DTilesDebug/views/3DTilesDebugWindow';
 export { ContributeModule } from './Contribute/ContributeModule';
 
 export { DocumentValidationModule } from './DocumentValidation/DocumentValidationModule';
+
+export { HelpWindow } from './Help/Help';
