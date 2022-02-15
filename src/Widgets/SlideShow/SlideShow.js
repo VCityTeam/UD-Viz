7d6da8ca49705ed7147712b03cd87d1ec25a75e6
mathieuLivebardon <tsunoll96@gmail.com>
2022-02-15 15:11:26 +0100

    clean slideshow (comments, method name...)

--- src/Widgets/SlideShow/SlideShow.js
+++ src/Widgets/SlideShow/SlideShow.js
@@ -63,29 +63,6 @@ export class SlideShow extends Window {
     }
   }
 
-  // GETTERS
-
-  /**returns coordinates HTMLElements (inputs+labels) */
-  get coordinatesInputVectorDOM() {
-    return document.getElementById(this.coordinatesInputVectorID);
-  }
-  /**return rotation HTMLElement (inputs+labels)*/
-  get rotationInputVectorDOM() {
-    return document.getElementById(this.rotationInputVectorID);
-  }
-  /**return size HTMLElement (inputs+labels)*/
-  get sizeInputVectorDOM() {
-    return document.getElementById(this.sizeInputVectorID);
-  }
-  /**return apspect ratio HTMLElement (checkbox)*/
-  get aspectRatioCheckboxDOM() {
-    return document.getElementById(this.aspectRatioCheckboxID);
-  }
-
-  get innerContentHtml() {
-    return this.htmlSlideShow.outerHTML;
-  }
-
   /**Create a default texture and fill the first texture file in this.texturesFiles */
   initDefaultTextureFile() {
     this.defaultTexture = new THREE.TextureLoader().load(
@@ -170,7 +147,7 @@ export class SlideShow extends Window {
       id: aspectRatioCheckbox.id,
       cb: function (event) {
         if (event.target.checked) {
-          const currentW = this.getSizeInputsValue().width;
+          const currentW = this.getSizeValues().width;
           const w =
             currentW != 0 ? currentW : this.currentTextureFile.getSize().width;
           this.setSizeInputs(new THREE.Vector2(null, w));
@@ -189,221 +166,6 @@ export class SlideShow extends Window {
     this.htmlSlideShow = htmlSlideShow;
   }
 
-  windowCreated() {
-    const _this = this;
-    this.callbacksHTMLEl.forEach(function (element) {
-      const htmlElement = document.getElementById(element.id);
-      htmlElement.addEventListener(element.event, element.cb.bind(_this));
-    });
-  }
-
-  /**Create 2 HTMLElements (title + inputVector) with labels list*/
-  createInputVector(labels, vectorName, step = 0.5) {
-    const titleVector = document.createElement('h3');
-    titleVector.innerHTML = vectorName;
-
-    const inputVector = document.createElement('div');
-    inputVector.id = vectorName + '_inputVector';
-    inputVector.style.display = 'grid';
-    for (let iInput = 0; iInput < labels.length; iInput++) {
-      const labelElement = document.createElement('label');
-      labelElement.innerHTML = labels[iInput];
-
-      const componentElement = document.createElement('input');
-      componentElement.id = vectorName + labelElement.innerHTML;
-      componentElement.type = 'number';
-      componentElement.setAttribute('value', '0');
-      componentElement.step = step;
-
-      labelElement.htmlFor = componentElement.id;
-      this.callbacksHTMLEl.push({
-        event: 'change',
-        id: componentElement.id,
-        cb: function (event) {
-          const value = event.target.value;
-          const element = event.target;
-          element.setAttribute('value', value);
-          if (this.aspectRatioCheckboxDOM.checked)
-            if (vectorName.toLowerCase().includes('size'))
-              this.matchRatio(iInput, value);
-          this.setVectors();
-        },
-      });
-
-      inputVector.appendChild(labelElement);
-      inputVector.appendChild(componentElement);
-    }
-    return {
-      title: titleVector,
-      inputVector: inputVector,
-    };
-  }
-
-  /**function called when aspectRatio is checked*/
-  matchRatio(iInput, value) {
-    const linkedSizeElement =
-      this.sizeInputVectorDOM.getElementsByTagName('input')[
-        iInput == 0 ? 1 : 0
-      ];
-
-    const height = this.currentTextureFile.getSize().height;
-    const width = this.currentTextureFile.getSize().width;
-    const ratio = width / height;
-    const newValue = iInput == 0 ? value / ratio : value * ratio;
-
-    linkedSizeElement.value = newValue;
-  }
-
-  /**set vectors variables with the values in the inputs HTMLElement*/
-  setVectors() {
-    this.coordinatesVector =
-      this.inputVectorToVector(this.coordinatesInputVectorDOM) ||
-      new THREE.Vector3();
-
-    this.rotationVector =
-      this.inputVectorToVector(this.rotationInputVectorDOM) ||
-      new THREE.Vector3();
-
-    this.sizeVector =
-      this.inputVectorToVector(this.sizeInputVectorDOM) || new THREE.Vector2();
-
-    this.modifyPlane();
-  }
-
-  /**convert inputVector HTMLElement to THREE.Vector*/
-  inputVectorToVector(inputVector) {
-    const inputEls = document
-      .getElementById(inputVector.id)
-      .getElementsByTagName('input');
-
-    const countEls = inputEls.length;
-
-    switch (countEls) {
-      case 2:
-        return new THREE.Vector2(inputEls[0].value, inputEls[1].value);
-      case 3:
-        return new THREE.Vector3(
-          inputEls[0].value,
-          inputEls[1].value,
-          inputEls[2].value
-        );
-      case 4:
-        return new THREE.Vector4(
-          inputEls[0].value,
-          inputEls[1].value,
-          inputEls[2].value,
-          inputEls[3].value
-        );
-    }
-
-    return null;
-  }
-
-  //SETTERS OF INPUTS ELEMENTS
-  setSizeInputs(vec2) {
-    const sizeInputEls = this.sizeInputVectorDOM.getElementsByTagName('input');
-
-    if (vec2.x !== null) {
-      const element0 = sizeInputEls[0];
-      element0.value = vec2.x;
-      element0.dispatchEvent(new Event('change'));
-    }
-
-    if (vec2.y !== null) {
-      const element1 = sizeInputEls[1];
-      element1.value = vec2.y;
-      element1.dispatchEvent(new Event('change'));
-    }
-  }
-
-  setCoordinatesInputs(vec3) {
-    const coordinatesInputEls =
-      this.coordinatesInputVectorDOM.getElementsByTagName('input');
-    const element0 = coordinatesInputEls[0];
-
-    if (vec3.x !== null) {
-      element0.value = vec3.x;
-      element0.dispatchEvent(new Event('change'));
-    }
-    if (vec3.y !== null) {
-      const element1 = coordinatesInputEls[1];
-      element1.value = vec3.y;
-      element1.dispatchEvent(new Event('change'));
-    }
-
-    if (vec3.z !== null) {
-      const element2 = coordinatesInputEls[2];
-      element2.value = vec3.z || this.coordinatesVector.z;
-      element2.dispatchEvent(new Event('change'));
-    }
-  }
-
-  setRotationInputs(vec3) {
-    const rotationInputEls =
-      this.rotationInputVectorDOM.getElementsByTagName('input');
-
-    if (vec3.x !== null) {
-      const element0 = rotationInputEls[0];
-      element0.value = vec3.x;
-      element0.dispatchEvent(new Event('change'));
-    }
-    if (vec3.y !== null) {
-      const element1 = rotationInputEls[1];
-      element1.value = vec3.y;
-      element1.dispatchEvent(new Event('change'));
-    }
-    if (vec3.z !== null) {
-      const element2 = rotationInputEls[2];
-      element2.value = vec3.z;
-      element2.dispatchEvent(new Event('change'));
-    }
-  }
-
-  //GETTERS of inputs HTMLElements
-  getSizeInputsValue() {
-    const sizeInputEls = this.sizeInputVectorDOM.getElementsByTagName('input');
-    return {
-      height: parseInt(sizeInputEls[0].value),
-      width: parseInt(sizeInputEls[1].value),
-    };
-  }
-
-  /**Create PlaneGeometry Mesh*/
-  createPlane() {
-    const geometry = new THREE.PlaneGeometry(1, 1);
-
-    const material = new THREE.MeshBasicMaterial({
-      map: this.defaultTexture,
-      side: THREE.DoubleSide,
-    });
-
-    this.plane = new THREE.Mesh(geometry, material);
-  }
-
-  /**Modify this.plane @var {THREE.Mesh} */
-  modifyPlane() {
-    if (!this.plane) {
-      this.createPlane();
-    }
-    this.plane.position.set(
-      this.coordinatesVector.x,
-      this.coordinatesVector.y,
-      this.coordinatesVector.z
-    );
-
-    this.plane.rotation.set(
-      this.rotationVector.x,
-      this.rotationVector.y,
-      this.rotationVector.z
-    );
-    this.plane.scale.set(this.sizeVector.x, this.sizeVector.y, 1);
-    this.plane.material.map = this.currentTexture || this.plane.material.map;
-
-    this.plane.updateMatrixWorld();
-    this.view.scene.add(this.plane);
-    this.app.update3DView();
-  }
-
   /**
    * @param {AllWidget} app
    * @param {InputManager} iM
@@ -415,12 +177,14 @@ export class SlideShow extends Window {
     // Clamp number between two values with the following line:
     const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
+    //Hide and show the geometryPlane
     iM.addKeyInput('h', 'keydown', function () {
       if (!_this.plane) return;
       _this.plane.visible = !_this.plane.visible;
       app.update3DView();
     });
 
+    //Change the next slide
     iM.addKeyInput('ArrowRight', 'keydown', function () {
       if (!_this.texturesFiles) return;
       _this.iCurrentText = clamp(
@@ -434,6 +198,8 @@ export class SlideShow extends Window {
 
       app.update3DView();
     });
+
+    //Change the previous slide
     iM.addKeyInput('ArrowLeft', 'keydown', function () {
       if (!_this.texturesFiles) return;
       _this.iCurrentText = clamp(
@@ -446,34 +212,6 @@ export class SlideShow extends Window {
 
       app.update3DView();
     });
-  }
-
-  /**
-   * @param {*} iText
-   * Set this.currentTexture
-   */
-  setTexture(iText) {
-    const _this = this;
-    if (this.currentTextureFile.video) {
-      this.currentTextureFile.video.pause();
-      this.currentTextureFile.video.currentTime = 0;
-      this.notifyValue = false;
-    }
-
-    this.texturesFiles.forEach(function (tf) {
-      if (tf.index == iText) {
-        _this.currentTextureFile = tf;
-        if (tf.video) {
-          tf.video.play();
-          _this.notifyValue = true;
-        }
-      }
-    });
-
-    this.currentTexture = this.currentTextureFile.texture;
-    const app = this.app;
-    this.modifyPlane();
-    app.update3DView();
   }
 
   /** Set the callback function of event 'drop' @warn !event.preventDefault! */
@@ -556,5 +294,274 @@ export class SlideShow extends Window {
       },
       false
     );
+  }
+
+  /**
+   * @param {Array.String} labels List of labels name
+   * @param {String} vectorName Name of the vector
+   * @param {number} step The step of HTMLElement input (type number)
+   * @returns {Object} title => HTMLElement 'h3' ; inputVector => HTMLElement 'div' contains labels and inputs HTMLElements
+   */
+  createInputVector(labels, vectorName, step = 0.5) {
+    const titleVector = document.createElement('h3');
+    titleVector.innerHTML = vectorName;
+
+    const inputVector = document.createElement('div');
+    inputVector.id = vectorName + '_inputVector';
+    inputVector.style.display = 'grid';
+    for (let iInput = 0; iInput < labels.length; iInput++) {
+      const labelElement = document.createElement('label');
+      labelElement.innerHTML = labels[iInput];
+
+      const componentElement = document.createElement('input');
+      componentElement.id = vectorName + labelElement.innerHTML;
+      componentElement.type = 'number';
+      componentElement.setAttribute('value', '0');
+      componentElement.step = step;
+
+      labelElement.htmlFor = componentElement.id;
+      this.callbacksHTMLEl.push({
+        event: 'change',
+        id: componentElement.id,
+        cb: function (event) {
+          const value = event.target.value;
+          const element = event.target;
+          element.setAttribute('value', value);
+          if (this.aspectRatioCheckboxDOM.checked)
+            if (vectorName.toLowerCase().includes('size'))
+              this.matchRatio(iInput, value);
+          this.updateVectors();
+        },
+      });
+
+      inputVector.appendChild(labelElement);
+      inputVector.appendChild(componentElement);
+    }
+    return {
+      title: titleVector,
+      inputVector: inputVector,
+    };
+  }
+
+  /**function called when aspectRatio is checked*/
+  matchRatio(iInput, value) {
+    const linkedSizeElement =
+      this.sizeInputVectorDOM.getElementsByTagName('input')[
+        iInput == 0 ? 1 : 0
+      ];
+
+    const height = this.currentTextureFile.getSize().height;
+    const width = this.currentTextureFile.getSize().width;
+    const ratio = width / height;
+    const newValue = iInput == 0 ? value / ratio : value * ratio;
+
+    linkedSizeElement.value = newValue;
+  }
+
+  /**Update vectors variables with the values contained in inputs elements in DOM */
+  updateVectors() {
+    this.coordinatesVector =
+      this.inputVectorToVector(this.coordinatesInputVectorDOM) ||
+      new THREE.Vector3();
+
+    this.rotationVector =
+      this.inputVectorToVector(this.rotationInputVectorDOM) ||
+      new THREE.Vector3();
+
+    this.sizeVector =
+      this.inputVectorToVector(this.sizeInputVectorDOM) || new THREE.Vector2();
+
+    this.modifyPlane();
+  }
+
+  /**convert inputVector HTMLElement to THREE.Vector*/
+  inputVectorToVector(inputVector) {
+    const inputEls = inputVector.getElementsByTagName('input');
+
+    const countEls = inputEls.length;
+
+    switch (countEls) {
+      case 2:
+        return new THREE.Vector2(inputEls[0].value, inputEls[1].value);
+      case 3:
+        return new THREE.Vector3(
+          inputEls[0].value,
+          inputEls[1].value,
+          inputEls[2].value
+        );
+      case 4:
+        return new THREE.Vector4(
+          inputEls[0].value,
+          inputEls[1].value,
+          inputEls[2].value,
+          inputEls[3].value
+        );
+    }
+
+    return null;
+  }
+
+  /**
+   * @param {*} iText
+   * Set this.currentTexture
+   */
+  setTexture(iText) {
+    const _this = this;
+    if (this.currentTextureFile.video) {
+      this.currentTextureFile.video.pause();
+      this.currentTextureFile.video.currentTime = 0;
+      this.notifyValue = false;
+    }
+
+    this.texturesFiles.forEach(function (tf) {
+      if (tf.index == iText) {
+        _this.currentTextureFile = tf;
+        if (tf.video) {
+          tf.video.play();
+          _this.notifyValue = true;
+        }
+      }
+    });
+
+    this.currentTexture = this.currentTextureFile.texture;
+    const app = this.app;
+    this.modifyPlane();
+    app.update3DView();
+  }
+
+  /**Modify this.plane @var {THREE.Mesh} */
+  modifyPlane() {
+    if (!this.plane) {
+      this.createPlane();
+    }
+    this.plane.position.set(
+      this.coordinatesVector.x,
+      this.coordinatesVector.y,
+      this.coordinatesVector.z
+    );
+
+    this.plane.rotation.set(
+      this.rotationVector.x,
+      this.rotationVector.y,
+      this.rotationVector.z
+    );
+    this.plane.scale.set(this.sizeVector.x, this.sizeVector.y, 1);
+    this.plane.material.map = this.currentTexture || this.plane.material.map;
+
+    this.plane.updateMatrixWorld();
+    this.view.scene.add(this.plane);
+    this.app.update3DView();
+  }
+
+  /**Create PlaneGeometry Mesh*/
+  createPlane() {
+    const geometry = new THREE.PlaneGeometry(1, 1);
+
+    const material = new THREE.MeshBasicMaterial({
+      map: this.defaultTexture,
+      side: THREE.DoubleSide,
+    });
+
+    this.plane = new THREE.Mesh(geometry, material);
+  }
+
+  //DOM GETTERS
+  /**returns coordinates HTMLElements (inputs+labels) */
+  get coordinatesInputVectorDOM() {
+    return document.getElementById(this.coordinatesInputVectorID);
+  }
+  /**return rotation HTMLElement (inputs+labels)*/
+  get rotationInputVectorDOM() {
+    return document.getElementById(this.rotationInputVectorID);
+  }
+  /**return size HTMLElement (inputs+labels)*/
+  get sizeInputVectorDOM() {
+    return document.getElementById(this.sizeInputVectorID);
+  }
+  /**return apspect ratio HTMLElement (checkbox)*/
+  get aspectRatioCheckboxDOM() {
+    return document.getElementById(this.aspectRatioCheckboxID);
+  }
+
+  get innerContentHtml() {
+    return this.htmlSlideShow.outerHTML;
+  }
+
+  //INPUTS ELEMENTS SETTERS
+  setSizeInputs(vec2) {
+    const sizeInputEls = this.sizeInputVectorDOM.getElementsByTagName('input');
+
+    if (vec2.x !== null) {
+      const element0 = sizeInputEls[0];
+      element0.value = vec2.x;
+      element0.dispatchEvent(new Event('change'));
+    }
+
+    if (vec2.y !== null) {
+      const element1 = sizeInputEls[1];
+      element1.value = vec2.y;
+      element1.dispatchEvent(new Event('change'));
+    }
+  }
+
+  setCoordinatesInputs(vec3) {
+    const coordinatesInputEls =
+      this.coordinatesInputVectorDOM.getElementsByTagName('input');
+
+    if (vec3.x !== null) {
+      const element0 = coordinatesInputEls[0];
+      element0.value = vec3.x;
+      element0.dispatchEvent(new Event('change'));
+    }
+    if (vec3.y !== null) {
+      const element1 = coordinatesInputEls[1];
+      element1.value = vec3.y;
+      element1.dispatchEvent(new Event('change'));
+    }
+
+    if (vec3.z !== null) {
+      const element2 = coordinatesInputEls[2];
+      element2.value = vec3.z || this.coordinatesVector.z;
+      element2.dispatchEvent(new Event('change'));
+    }
+  }
+
+  setRotationInputs(vec3) {
+    const rotationInputEls =
+      this.rotationInputVectorDOM.getElementsByTagName('input');
+
+    if (vec3.x !== null) {
+      const element0 = rotationInputEls[0];
+      element0.value = vec3.x;
+      element0.dispatchEvent(new Event('change'));
+    }
+    if (vec3.y !== null) {
+      const element1 = rotationInputEls[1];
+      element1.value = vec3.y;
+      element1.dispatchEvent(new Event('change'));
+    }
+    if (vec3.z !== null) {
+      const element2 = rotationInputEls[2];
+      element2.value = vec3.z;
+      element2.dispatchEvent(new Event('change'));
+    }
+  }
+
+  /**Get size values contained in inputs elements in DOM*/
+  getSizeValues() {
+    const sizeInputEls = this.sizeInputVectorDOM.getElementsByTagName('input');
+    return {
+      height: parseInt(sizeInputEls[0].value),
+      width: parseInt(sizeInputEls[1].value),
+    };
+  }
+
+  windowCreated() {
+    const _this = this;
+    // Through this.callbacksHTMLEl and addEventListeners to HTMLElements in DOM (elements which created by Window class)
+    this.callbacksHTMLEl.forEach(function (element) {
+      const htmlElement = document.getElementById(element.id);
+      htmlElement.addEventListener(element.event, element.cb.bind(_this));
+    });
   }
 }
