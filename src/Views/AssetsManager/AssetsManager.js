c198cdcdf4f2b0aefd3b0047b0598ef2bd16da35
Mathieu Livebardon <tsunoll96@gmail.com>
2022-03-25 17:06:49 +0100

    import script

--- src/Views/AssetsManager/AssetsManager.js
+++ src/Views/AssetsManager/AssetsManager.js
@@ -15,7 +15,6 @@ const DEFAULT_MATERIAL = new THREE.MeshLambertMaterial({
 });
 
 import './AssetsManager.css';
-// const module = __unused_webpack_module;
 
 /**
  * Give acess to all assets (image, video, script, worlds, ...)
@@ -410,6 +409,7 @@ export class AssetsManager {
       let resultRequire = string.replace(regexRequire, '');
       return resultRequire.replace(regexType, '');
     };
+    const importScript = "const module = import('./AssetsManager.js');";
 
     if (config.worldScripts) {
       const idLoadingWorldScripts = 'WorldScripts';
@@ -424,7 +424,9 @@ export class AssetsManager {
               scriptPath,
               function (scriptString) {
                 scriptString = toEvalCode(scriptString);
-                _this.worldScripts[idScript] = eval(scriptString);
+                _this.worldScripts[idScript] = eval(
+                  importScript + scriptString
+                );
                 //check if finish
                 count++;
 
@@ -458,7 +460,9 @@ export class AssetsManager {
               scriptPath,
               function (scriptString) {
                 scriptString = toEvalCode(scriptString);
-                _this.localScripts[idScript] = eval(scriptString);
+                _this.localScripts[idScript] = eval(
+                  importScript + scriptString
+                );
                 //check if finish
                 count++;
 
