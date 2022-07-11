2250586b031d11f2d711ca19f0063d4c27be6155
CorentinLIRIS <corentin.chignin@hotmail.fr>
2022-07-11 16:57:41 +0200

    Add file for navigation widget

--- src/Widgets/Extensions/Navigation/Navigation.js
+++ src/Widgets/Extensions/Navigation/Navigation.js
@@ -10,6 +10,8 @@ export class Navigation extends ModuleView {
     let renderer = view3D.mainLoop.gfxEngine.renderer;
     //Zoom value for the slider
     let zoomValue = 100;
+    
+    const camera = view3D.camera.camera3D;
 
     //3D scene of your camera
     let scene3D =  view3D.scene;
@@ -22,7 +24,7 @@ export class Navigation extends ModuleView {
         <h3 id="zoomMoins">-</h3>\
         <input type="range" min="1" max="100" value="50" class="slider" id="myRange">\
         <h3 id="zoomPlus">+</h3>\
-        <img src="./compass.png" id="compass" style="transform: rotate(-43.4214deg);">';
+        <img src="../../../../examples/assets/img/compass.png" id="compass" style="transform: rotate(-43.4214deg);">';
     document.getElementById('contentSection').append(zoomDiv);
 
     let rangeslider = document.getElementById('myRange');
@@ -36,11 +38,11 @@ export class Navigation extends ModuleView {
         scaleZoom = -zoomValue;
       }
       let direction = new THREE.Vector3();
-      view3D.camera.getWorldDirection(direction);
-      let camera = view3D.camera.position;
+      camera.getWorldDirection(direction);
+      let cameraPos = camera.position;
           
       direction =  new THREE.Vector3(direction.x * scaleZoom, direction.y * scaleZoom, direction.z * scaleZoom);
-      camera.set(camera.x + direction.x, camera.y + direction.y, camera.z + direction.z);
+      cameraPos.set(cameraPos.x + direction.x, cameraPos.y + direction.y, cameraPos.z + direction.z);
       lastValue = this.value;
     };
 
@@ -48,8 +50,8 @@ export class Navigation extends ModuleView {
     var dir = new THREE.Vector3();
     var sph = new THREE.Spherical();
     renderer.setAnimationLoop(() => {
-      renderer.render(scene3D, view3D.camera);
-      view3D.camera.getWorldDirection(dir);
+      renderer.render(scene3D, camera);
+      camera.getWorldDirection(dir);
       sph.setFromVector3(dir);
       document.getElementById('compass').style.transform = `rotate(${THREE.Math.radToDeg(sph.theta) - 180}deg)`;
     });
