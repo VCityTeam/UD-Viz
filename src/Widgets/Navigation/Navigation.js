37118a829aa926149b596454a14cd9bed37081d4
CorentinLIRIS <corentin.chignin@hotmail.fr>
2022-07-15 11:43:36 +0200

    Fix compass rotation

--- src/Widgets/Navigation/Navigation.js
+++ src/Widgets/Navigation/Navigation.js
@@ -13,10 +13,7 @@ export class Navigation extends ModuleView {
     //Zoom value for the slider
     let zoomValue = 100;
     
-    //3D scene of your camera
-    let scene3D =  view3D.scene;
-     
-    console.log(view3D);
+    //Itowns camera 
     const camera = view3D.camera.camera3D;
 
     // Zoom UI
@@ -27,7 +24,7 @@ export class Navigation extends ModuleView {
         <h3 id="zoomMoins">-</h3>\
         <input type="range" min="1" max="100" value="50" class="slider" id="myRange">\
         <h3 id="zoomPlus">+</h3>\
-        <img src="../../../../examples/assets/img/compass.png" id="compass" style="transform: rotate(-43.4214deg);">';
+        <img src="../../../../examples/assets/img/compass.png" id="compass" style="transform: rotate(0deg);">';
     document.getElementById('contentSection').append(zoomDiv);
 
     let rangeslider = document.getElementById('myRange');
@@ -50,13 +47,10 @@ export class Navigation extends ModuleView {
     };
 
     //Compass update with camera
-    let dir = new THREE.Vector3();
-    let sph = new THREE.Spherical();
     renderer.setAnimationLoop(() => {
-      camera.getWorldDirection(dir);
       view3D.notifyChange();
-      sph.setFromVector3(dir);
-      document.getElementById('compass').style.transform = `rotate(${THREE.Math.radToDeg(sph.theta) - 180}deg)`;
+      //Get z rotation of the camera and convert in degree
+      document.getElementById('compass').style.transform = `rotate(${-THREE.Math.radToDeg(camera.rotation.z)}deg)`;
     });
   }
 
