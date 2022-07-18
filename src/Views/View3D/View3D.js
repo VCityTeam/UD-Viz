0c7e440d36371aac803711467054538460e3c04d
Diego Vinasco <diego.vinasco@protonmail.com>
2022-07-18 18:55:40 +0200

    migrate addgeojsonlayer function to view3d

--- src/Views/View3D/View3D.js
+++ src/Views/View3D/View3D.js
@@ -14,6 +14,7 @@ import { LayerManager } from '../../Components/Components';
 import { addBaseMapLayer } from '../../Components/Components';
 import { addElevationLayer } from '../../Components/Components';
 import { setupAndAdd3DTilesLayers } from '../../Components/Components';
+import { setupAndAddGeoJsonLayers } from '../../Components/Components';
 
 /**
  *  Main view of an ud-viz application
@@ -386,6 +387,8 @@ export class View3D {
     addBaseMapLayer(this.config, this.itownsView, this.extent);
     addElevationLayer(this.config, this.itownsView, this.extent);
     setupAndAdd3DTilesLayers(this.config, this.layerManager, this.itownsView);
+    setupAndAddGeoJsonLayers(this.config, this.itownsView);
+
 
     //disable itowns resize
     this.itownsViewResize = this.itownsView.resize.bind(this.itownsView);
