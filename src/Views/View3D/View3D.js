b00e71e4039b2fe852ee9aff0bc8f4e05d563a41
AdrienHrdz <adrienhernandez16@gmail.com>
2022-09-08 15:53:06 +0200

    del useless id

--- src/Views/View3D/View3D.js
+++ src/Views/View3D/View3D.js
@@ -50,7 +50,6 @@ export class View3D {
     // Ui
     this.ui = document.createElement('div');
     this.ui.classList.add('ui_View3D');
-    this.ui.setAttribute('id', 'ui_View3D');
     this.rootWebGL.appendChild(this.ui);
 
     // Listen resize event
