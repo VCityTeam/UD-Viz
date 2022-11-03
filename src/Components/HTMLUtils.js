2d38b250996b83b609fd8e8438ad1b28cf56eaa3
valentinMachado <valentin.machado.cpe@gmail.com>
2022-11-03 15:26:28 +0100

    warning + eslint + fix billboard toCss method

--- src/Components/HTMLUtils.js
+++ src/Components/HTMLUtils.js
@@ -1,8 +1,9 @@
 /**
  * Check if an html element belong to another one recursively
+ *
  * @param {HTMLElement} child the html child
  * @param {HTMLElement} parent the html parent
- * @returns {Boolean}
+ * @returns {boolean}
  */
 export function checkParentChild(child, parent) {
   let currentNode = child;
