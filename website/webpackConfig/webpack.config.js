0942ae4ecbbc2096a6c22a1ff24c800fc1063e88
valentinMachado <valentin.machado.cpe@gmail.com>
2022-12-06 11:05:03 +0100

    WIP website

---
+++ website/webpackConfig/webpack.config.js
@@ -0,0 +1,9 @@
+const { merge } = require('webpack-merge');
+
+const envConfig =
+  process.env.NODE_ENV === 'production'
+    ? require('./webpack.config.prod')
+    : require('./webpack.config.dev');
+
+// We use this here at edX
+module.exports = merge(require('./webpack.config.common'), envConfig);
