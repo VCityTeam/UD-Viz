912c298cbc7400ec9ff5bc32885bcc797017482c
valentinMachado <valentin.machado.cpe@gmail.com>
2022-12-06 17:52:02 +0100

    structure website okay

--- website/src/index.js
+++
@@ -1,5 +0,0 @@
-import './style.css';
-import { ReceptionView } from './Reception/Reception';
-
-const receptionView = new ReceptionView();
-document.body.appendChild(receptionView.html());
--- website/webpackConfig/webpack.config.prod.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  mode: 'production',
-  output: {
-    path: path.resolve(__dirname, '../dist/release'),
-  },
-};
