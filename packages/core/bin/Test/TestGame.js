48efcb061622f7a1782c557b522a74ca7621ab6e
valentinMachado <valentin.machado.cpe@gmail.com>
2022-12-19 12:02:31 +0100

    info test

---
+++ packages/core/bin/Test/TestGame.js
@@ -0,0 +1,5 @@
+//create a gameprocess
+//create a controllerprocess
+//controller send command to gameprocess to move object which are are going to collide with another one
+//when the collision happen gameprocess edit variables of an externalscript
+//controller process check the change of the variables of the externalscript to exit 0
