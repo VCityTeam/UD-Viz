57d34a34fb71910c21136ae00c9e1649171b705d
valentinMachado <valentin.machado.cpe@gmail.com>
2022-12-19 18:19:52 +0100

    reorganise View folder

---
+++ packages/browser/src/View/Planar.js
@@ -0,0 +1,11 @@
+import {
+  LayerManager,
+  addBaseMapLayer,
+  addElevationLayer,
+  add3DTilesLayersFromConfig,
+  setupAndAddGeoJsonLayers,
+  checkParentChild,
+} from '../../Components/Components';
+
+
+import { computeNearFarCamera } from '../';
