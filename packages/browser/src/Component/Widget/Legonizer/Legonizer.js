b629029b96fb90322bb736654c7605d7d8f9bec4
CorentinLIRIS <corentin.chignin@hotmail.fr>
2022-12-21 11:38:59 +0100

    Create window legonizer

--- packages/browser/src/Component/Widget/Legonizer/Legonizer.js
+++ packages/browser/src/Component/Widget/Legonizer/Legonizer.js
@@ -1,54 +1,65 @@
 /** @format */
 
 // Components
-import { ModuleView } from '../Components/ModuleView/ModuleView';
-import jQuery from 'jquery';
+import { Window } from '../Components/GUI/js/Window';
+// Import jQuery from 'jquery';
 import './../About/About.css';
 
 /**
  *
  *
  */
-export class LegonizerWindow extends ModuleView {
-  constructor(config = {}) {
-    super();
-    this.config = config;
+export class LegonizerWindow extends Window {
+  constructor() {
+    super('legonizer', 'Legonizer', false);
+  }
+
+  get innerContentHtml() {
+    return `
+    <div id="${this.paramLegonizerId}">
+      <div class="box-section" id="${this.coordBoxSectionId}"> 
+        <label for="color-layers-spoiler" class="section-title">Coordinates</Label>
+      </div>
+      <div class="box-section" id="${this.scaleSectionId}"> 
+        <label for="elevation-layers-spoiler" class="section-title">Scales parameters</Label>
+      </div>
+    </div>`;
   }
 
   // ///// MODULE VIEW MANAGEMENT
-  enableView() {
-    const widgetlayout = document.getElementById('_window_widget_content');
-    widgetlayout.style.setProperty('display', 'block');
-    widgetlayout.innerHTML = '';
-    // Create HMTL
-    const promises = [];
-    if (this.config.htmlPaths && this.config.htmlPaths.length) {
-      this.config.htmlPaths.forEach(function (path) {
-        promises.push(
-          new Promise((resolve, reject) => {
-            jQuery.ajax({
-              type: 'GET',
-              url: path,
-              datatype: 'html',
-              success: (data) => {
-                widgetlayout.innerHTML += data;
-                resolve();
-              },
-              error: (e) => {
-                console.error(e);
-                reject();
-              },
-            });
-          })
-        );
-      });
-    }
-  }
+  // enableView() {
+  //   const widgetlayout = document.getElementById('_window_widget_content');
+  //   widgetlayout.style.setProperty('display', 'block');
+  //   widgetlayout.innerHTML = '';
+  //   // Create HMTL
+  //   const promises = [];
+  //   if (this.config.htmlPaths && this.config.htmlPaths.length) {
+  //     this.config.htmlPaths.forEach(function (path) {
+  //       promises.push(
+  //         new Promise((resolve, reject) => {
+  //           jQuery.ajax({
+  //             type: 'GET',
+  //             url: path,
+  //             datatype: 'html',
+  //             success: (data) => {
+  //               widgetlayout.innerHTML += data;
+  //               resolve();
+  //             },
+  //             error: (e) => {
+  //               console.error(e);
+  //               reject();
+  //             },
+  //           });
+  //         })
+  //       );
+  //     });
+  //   }
+  // }
 
-  disableView() {
-    document
-      .getElementById('_window_widget_content')
-      .style.setProperty('display', 'none');
-    document.getElementById('_window_widget_content').innerHTML = '';
-  }
+  // disableView() {
+  //   document
+  //     .getElementById('_window_widget_content')
+  //     .style.setProperty('display', 'none');
+  //   document.getElementById('_window_widget_content').innerHTML = '';
+  // }
 }
