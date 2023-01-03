6f00575b21cd1e8f320c961757cdb73d9384daf5
CorentinLIRIS <corentin.chignin@hotmail.fr>
2023-01-03 09:57:29 +0100

    Add scale parameters UI

--- src/Widgets/Legonizer/Legonizer.js
+++ src/Widgets/Legonizer/Legonizer.js
@@ -10,8 +10,10 @@ import './../About/About.css';
  *
  */
 export class LegonizerWindow extends Window {
-  constructor() {
+  constructor(view3D) {
     super('legonizer', 'Legonizer', false);
+
+    this.view3D = view3D;
   }
 
   get innerContentHtml() {
@@ -60,7 +62,7 @@ export class LegonizerWindow extends Window {
 
     this.coordBoxElement.appendChild(inputVector);
 
-    // Button Selecte an area
+    // Button Select an area
     const buttonSelectionAreaElement = document.createElement('button');
     buttonSelectionAreaElement.id = 'button_selection';
     buttonSelectionAreaElement.textContent = 'Select an area';
@@ -77,14 +79,61 @@ export class LegonizerWindow extends Window {
     this.parametersElement.appendChild(buttonGenerateMockupElement);
   }
 
+  innerContentScale() {
+    const inputVector = document.createElement('div');
+    inputVector.id = 'lego' + '_inputVector';
+    inputVector.style.display = 'inline-flex';
+
+    const coordinatesString = ['x count lego plate', 'y count lego plate'];
+
+    for (let i = 0; i < 2; i++) {
+      // //coord Elements
+      const scaleElement = document.createElement('div');
+      scaleElement.id = coordinatesString[i] + '_grid';
+      scaleElement.style.display = 'grid';
+      scaleElement.style.width = '50%';
+      scaleElement.style.height = 'auto';
+
+      // Label
+      const labelElement = document.createElement('h3');
+      labelElement.textContent = coordinatesString[i];
+
+      // Input
+      const inputElement = document.createElement('input');
+      inputElement.id = 'input' + coordinatesString[i];
+      inputElement.type = 'number';
+      inputElement.style.width = 'inherit';
+      inputElement.setAttribute('value', '0');
+
+      scaleElement.appendChild(labelElement);
+      scaleElement.appendChild(inputElement);
+
+      inputVector.appendChild(scaleElement);
+    }
+
+    this.scaleBoxElement.appendChild(inputVector);
+  }
+
   windowCreated() {
     this.innerContentCoordinates();
+    this.innerContentScale();
+    const geometry = new THREE.BoxGeometry(200, 200, 200);
+    const object = new THREE.Mesh(
+      geometry,
+      new THREE.MeshLambertMaterial({ color: 0x00ff00 })
+    );
+
+    object.position.x = this.view3D.extent.center().x;
+    object.position.y = this.view3D.extent.center().y;
+    object.position.z = this.view3D.extent.center().z;
+
+    this.view3D.getItownsView().scene.add(object);
   }
 
   generateMockup() {
     // Create THREE js window with heightmap mesh
     const mockupWindow = new Window('MockupWindow', 'Mockup Window', true);
-    // mockupWindow.innerContentHtml = `<div id="lego_mockup_window">`;
+    // MockupWindow.innerContentHtml = `<div id="lego_mockup_window">`;
     const mockupElement = mockupWindow.header;
     console.log(mockupElement);
     mockupElement.style.left = 'unset';
@@ -102,6 +151,10 @@ export class LegonizerWindow extends Window {
     return `box_section_coordinates`;
   }
 
+  get scaleSectionId() {
+    return `box_section_scale`;
+  }
+
   get paramLegonizerId() {
     return `div_parameters`;
   }
@@ -112,5 +165,9 @@ export class LegonizerWindow extends Window {
 
   get parametersElement() {
     return document.getElementById(this.paramLegonizerId);
+  }
+
+  get scaleBoxElement() {
+    return document.getElementById(this.scaleSectionId);
   }
 }
