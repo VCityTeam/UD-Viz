275a8fe82953ebee8c4b17077827d9f81fd9740a
Mathieu Livebardon <tsunoll96@gmail.com>
2023-01-25 10:32:49 +0100

    some doc Itowns

--- packages/browser/src/Component/Itowns/Itowns.js
+++ packages/browser/src/Component/Itowns/Itowns.js
@@ -1,3 +1,13 @@
+/**
+ * MODULE Itowns
+ *
+ * @module @ud-viz/browser/Itowns
+ * @see LayerManager
+ * @see TilesManager
+ * @see module:@ud-viz/browser/Components
+ * @see {@link http://www.itowns-project.org/itowns/docs/#home}
+ */
+
 export { LayerManager } from './LayerManager/LayerManager';
 
 export { TilesManager } from './3DTiles/TilesManager';
