cfefb93716908604a2a8b247c422dfff2aadba68
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2023-02-08 17:33:30 +0100

    merge conflicts

--- packages/browser/src/Component/Widget/Server/Documents/View/DocumentInspectorWindow.js
+++ packages/browser/src/Component/Widget/Server/Documents/View/DocumentInspectorWindow.js
@@ -25,7 +25,14 @@ export class DocumentInspectorWindow extends AbstractDocumentWindow {
      * Represents a list of extensions. An extension can either be a button or
      * a panel.
      *
-     * @type {Object<string, DocumentInspectorExtension>}
+     * @type {Object<string, {
+     *  type: 'button' | 'panel',
+     *  container?: string,
+     *  label: string,
+     *  id: string,
+     *  callback?: (doc: Document) => any,
+     *  html: string
+     * }>}
      */
     this.extensions = {};
   }
--- packages/browser/src/Component/Widget/Server/Documents/View/DocumentNavigatorWindow.js
+++ packages/browser/src/Component/Widget/Server/Documents/View/DocumentNavigatorWindow.js
@@ -32,7 +32,13 @@ export class DocumentNavigatorWindow extends AbstractDocumentWindow {
      * Represents a list of extensions. An extension can either be a button or
      * a panel.
      *
-     * @type {Object<string, DocumentNavigatorExtension>}
+     * @type {Object<string, {
+     *  type: 'button' | 'panel',
+     *  label: string,
+     *  id: string,
+     *  callback?: (doc: Document[]) => any,
+     *  html: string
+     * }>}
      */
     this.extensions = {};
   }
