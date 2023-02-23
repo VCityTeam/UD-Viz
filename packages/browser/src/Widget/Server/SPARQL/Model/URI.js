6f14530c033d1140e929b466e342b368311dd9c1
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2023-02-23 11:19:49 +0100

    refactor uri functions and fix timestamp bug

--- packages/browser/src/Widget/Server/SPARQL/Model/URI.js
+++ packages/browser/src/Widget/Server/SPARQL/Model/URI.js
@@ -1,5 +1,8 @@
 /**
  * Tokenize a URI into a namespace and localname
+ * A uri is typically composed of a [namespace]#[localname]
+ * e.g. http://site.io/test#example_1
+ * here the namespace is 'http://site.io/test#' and the localname is 'example_1'
  *
  * @param {string} uri The URI to be tokenized
  * @returns {{namespace:string, localname:string}} object of the URI tokenized
@@ -18,4 +21,26 @@ export function tokenizeURI(uri) {
     tokenizedURI.namespace = uriTokens.join('/');
   }
   return tokenizedURI;
+}
+
+/**
+ * Return the localname of a URI
+ *
+ * @param {string} uri The URI
+ * @returns {string} the localname of the URI
+ */
+export function getUriLocalname(uri) {
+  const uriTokens = tokenizeURI(uri);
+  return uriTokens.localname;
+}
+
+/**
+ * Return the namespace of a URI
+ *
+ * @param {string} uri The URI
+ * @returns {string} the namespace of the URI
+ */
+export function getUriNamespace(uri) {
+  const uriTokens = tokenizeURI(uri);
+  return uriTokens.namespace;
 }
