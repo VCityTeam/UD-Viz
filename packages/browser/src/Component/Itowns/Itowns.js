79a9daf584f0bfbaced74515b952377ceba0070d
Clément COLIN <clement.colin69@gmail.com>
2023-03-08 16:31:27 +0100

    merge

--- packages/browser/src/Component/Itowns/Itowns.js
+++ packages/browser/src/Component/Itowns/Itowns.js
@@ -11,12 +11,6 @@ export { LayerManager } from './LayerManager/LayerManager';
 /** @class TilesManager */
 export { TilesManager } from './3DTiles/TilesManager';
 
-/**
- * @class CityObject
- * @class CityObjectID
- */
-export { CityObject, CityObjectID } from './3DTiles/Model/CityObject';
-
 import * as Component from './Component/Component';
 /** @member {module:udvizBrowser/Itowns/Component} Component */
 export { Component };
