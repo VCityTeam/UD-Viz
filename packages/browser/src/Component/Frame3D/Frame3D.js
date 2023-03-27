e9e739e3120b8de6349ca6dca6b67bea60bc2149
valentinMachado <valentin.machado.cpe@gmail.com>
2023-03-27 17:42:26 +0200

    WIP rebase

--- packages/browser/src/Component/Frame3D/Frame3D.js
+++ packages/browser/src/Component/Frame3D/Frame3D.js
@@ -2,5 +2,4 @@ export { Frame3DPlanar } from './Frame3DPlanar';
 
 export { Frame3DBase } from './Frame3DBase/Frame3DBase';
 
-import * as Component from './Component/Component';
-export { Component };
+export * from './Component/Component';
--- packages/browser/src/Component/Game/External/ExternalGame.js
+++ packages/browser/src/Component/Game/External/ExternalGame.js
@@ -2,5 +2,4 @@ export { Context, ExternalScriptBase as ScriptBase } from './Context';
 export { MultiPlanarProcess } from './MultiPlanarProcess';
 export { SinglePlanarProcess } from './SinglePlanarProcess';
 
-import * as ScriptTemplate from './ScriptTemplate/ScriptTemplate';
-export { ScriptTemplate };
+export * from './ScriptTemplate/ScriptTemplate';
