51ae7af986cf3b8cf825a38d9d98bdcdf358cec4
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2023-04-05 18:51:07 +0200

    add blazegraph query json format parameter

--- packages/browser/src/Component/Widget/Server/SPARQL/Model/SparqlEngineRequest.js
+++ packages/browser/src/Component/Widget/Server/SPARQL/Model/SparqlEngineRequest.js
@@ -25,7 +25,7 @@ export class SparqlEngineRequest {
             case 'blazegraph':
                 // http://localhost:8080/bigdata/sparql?query=SELECT%20*%20%7B%20%3Fs%20%3Fp%20%3Fo%20%7D
             default:
-                return this.config.url + 'bigdata/sparql' + '?query=' + encodeURIComponent(this.config.query);
+                return this.config.url + 'bigdata/sparql' + '?format=json&query=' + encodeURIComponent(this.config.query);
         }
     }
 
