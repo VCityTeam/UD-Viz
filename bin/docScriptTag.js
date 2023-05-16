09006e6ec84f673183846de0d6cc616587aa7c94
valentinMachado <valentin.machado.cpe@gmail.com>
2023-05-16 12:06:13 +0200

    feat(doc) script to comment/uncomment script tag

--- bin/docScriptTag.js
+++ bin/docScriptTag.js
@@ -1,7 +1,9 @@
+/** @file If first argument is `include` script tag are uncommented if nothing is passed script tag are commented */
+
 const fs = require('fs');
 const { Data } = require('@ud-viz/shared');
 
-const commentScriptTag = process.argv[2] == 'include';
+const includeScriptTag = process.argv[2] == 'include';
 
 const parseDirectory = (directoryPath) => {
   const dirents = fs.readdirSync(directoryPath, { withFileTypes: true });
@@ -9,16 +11,31 @@ const parseDirectory = (directoryPath) => {
     if (dirent.isFile() && Data.computeFileFormat(dirent.name) == 'md') {
       const filePath = directoryPath + '/' + dirent.name;
 
-      const contentMd = fs.readFileSync(filePath, {
+      // read contents of the file
+      const data = fs.readFileSync(filePath, {
         encoding: 'utf-8',
       });
 
-      console.log(filePath);
+      if (!includeScriptTag) {
+        fs.writeFileSync(
+          filePath,
+          data
+            .replace(/<script/g, '<!-- <script')
+            .replace(/script>/g, 'script> -->')
+        );
+      } else {
+        fs.writeFileSync(
+          filePath,
+          data
+            .replace(/<!-- <script/g, '<script')
+            .replace(/script> -->/g, 'script>')
+        );
+      }
     } else if (dirent.isDirectory()) {
       parseDirectory(directoryPath + '/' + dirent.name); // recursive
     }
   });
 };
 
-console.log(`\nComment script tag: ${commentScriptTag}\n`);
+console.log(`\nInclude script tag: ${includeScriptTag}\n`);
 parseDirectory('./docs/static');
