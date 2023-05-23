1f6bf21ef5d524b1a11a6ac39bf9aa70ae1d162b
valentinMachado <valentin.machado.cpe@gmail.com>
2023-05-23 11:24:01 +0200

    fix(CI) paths

--- packages/browser/bin/test.js
+++ packages/browser/bin/test.js
@@ -2,9 +2,14 @@ const Test = require('@ud-viz/node').Test;
 
 console.log('@ud-viz/browser test start');
 
+const bundlePath =
+  process.env.NODE_ENV === 'production'
+    ? './dist/release/bundle.js'
+    : './dist/debug/bundle.js';
+
 /**
  * ./dist/debug/bundle.js must have been builded
  */
-Test.browserScripts('./bin/Test', './dist/debug/bundle.js').catch((error) => {
+Test.browserScripts('./bin/Test', bundlePath).catch((error) => {
   console.error(error);
 });
