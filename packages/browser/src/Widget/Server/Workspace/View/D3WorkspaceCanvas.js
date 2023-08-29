284dfa893d3eaa7796314807c4a6259ec2e0d52a
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2023-08-29 19:59:09 +0200

    add workspace graph event handler

--- packages/browser/src/Widget/Server/Workspace/View/D3WorkspaceCanvas.js
+++ packages/browser/src/Widget/Server/Workspace/View/D3WorkspaceCanvas.js
@@ -1,4 +1,3 @@
-import { SparqlQueryWindow } from '../../SPARQL/View/SparqlQueryWindow';
 import { D3GraphCanvas } from '../../SPARQL/View/D3GraphCanvas';
 import { Workspace } from '../Model/Workspace';
 
@@ -8,7 +7,6 @@ export class D3WorkspaceCanvas extends D3GraphCanvas {
    * Adapted from https://observablehq.com/@d3/force-directed-graph#chart and
    * https://www.d3indepth.com/zoom-and-pan/
    *
-   * @param {SparqlQueryWindow} window the window this graph is attached to.
    * @param {object} configSparqlWidget The sparqlModule configuration.
    * @param {number} configSparqlWidget.height The SVG canvas height.
    * @param {number} configSparqlWidget.width The SVG canvas width.
@@ -16,8 +14,8 @@ export class D3WorkspaceCanvas extends D3GraphCanvas {
    * @param {object} configSparqlWidget.namespaceLabels Prefix declarations which will replace text labels in the Legend.
    *                                                    This doesn't (yet) affect the legend font size.
    */
-  constructor(window, configSparqlWidget) {
-    super(window, configSparqlWidget);
+  constructor(configSparqlWidget) {
+    super(configSparqlWidget);
     this.data = new Workspace();
   }
 }
