d30f75d2d84ece0adcdf97d9b0476271c8b1529c
CorentinLIRIS <corentin.chignin@hotmail.fr>
2023-09-06 13:21:50 +0200

    Setup Linked list

--- packages/browser/src/Widget/Temporal/SpaceTimeCube.js
+++ packages/browser/src/Widget/Temporal/SpaceTimeCube.js
@@ -1,6 +1,40 @@
 import * as itowns from 'itowns';
 import * as THREE from 'three';
 
+const TEMPORAL_COLOR_OPACITY = {
+  noTransaction: {
+    color: 'white',
+    opacity: 1,
+    priority: 1,
+  },
+  invisible: {
+    color: 'blue',
+    opacity: 0.2,
+    priority: 0,
+  },
+  debug: {
+    color: 'brown',
+    opacity: 0.2,
+    priority: 1,
+  },
+  creation: {
+    color: 'green',
+    opacity: 0.6,
+    priority: 1,
+  },
+  demolition: {
+    color: 'red',
+    opacity: 0.6,
+    priority: 1.5,
+  },
+  modification: {
+    color: 'yellow',
+    opacity: 0.6,
+    priority: 1,
+  },
+};
+
+// TO-DO: list chainé pour connaitre le level en dessous.
 class Level {
   constructor(date, boundingBox, features) {
     /** @type {THREE.Box3} */
@@ -76,6 +110,58 @@ class Level {
   }
 }
 
+class Node {
+  /**
+   *
+   * @param {Level} level
+   */
+  constructor(level) {
+    this.level = level;
+    this.next = null;
+  }
+}
+
+class LevelList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  /**
+   *
+   * @param {Level} level
+   */
+  add(level) {
+    const node = new Node(level);
+
+    let current;
+
+    if (this.head == null) this.head = node;
+    else {
+      current = this.head;
+      while (current.next) {
+        console.log(current.next);
+        current = current.next;
+      }
+
+      current.next = current;
+    }
+    this.size++;
+  }
+
+  // finds the index of element
+  containNode(level) {
+    let current = this.head;
+
+    while (current != null) {
+      if (current.level.date === level.date) return true;
+      current = current.next;
+    }
+
+    // not found
+    return false;
+  }
+}
 export class SpaceTimeCube {
   /**
    *
@@ -84,6 +170,8 @@ export class SpaceTimeCube {
   constructor(view) {
     /** @type {Map<number,Level>} */
     const levels = new Map();
+
+    const levelsList = new LevelList();
 
     const temporalLayers = () => {
       return view.getLayers().filter((el) => {
@@ -98,6 +186,11 @@ export class SpaceTimeCube {
       temporalLayer.addEventListener(
         itowns.C3DTILES_LAYER_EVENTS.ON_TILE_CONTENT_LOADED,
         () => {
+          temporalLayer.tileset.extensions[
+            '3DTILES_temporal'
+          ].transactions.forEach((transaction) => {});
+
+          // Features
           for (const [
             tileId,
             tileFeatures,
@@ -118,6 +211,9 @@ export class SpaceTimeCube {
                   [feature]
                 );
                 levels.set(date, newLevel);
+                // if (!levelsList.containNode(newLevel)) levelsList.add(newLevel);
+
+                // levelsList.add(newLevel);
 
                 view.scene.add(newLevel.boudingBoxHelper);
               } else {
@@ -136,7 +232,6 @@ export class SpaceTimeCube {
             // TODO : ca marche pas le level.height il faut tenir compte des levels en dessous
             level.offset = (date - minDate) * 100;
           }
-          console.log(levels);
 
           temporalLayers().forEach((layer) => {
             for (const [tileId, tileFeatures] of layer.tilesC3DTileFeatures) {
@@ -172,15 +267,36 @@ export class SpaceTimeCube {
                       ) {
                         child.geometry.attributes.position.array[index + 2] +=
                           -oldOffset + level.offset;
+
+                        // TO-DO: dupliquer la géometry quand c'est du construction ou demolition
                       }
                     });
                   }
                 });
               }
             }
+
+            layer.style = new itowns.Style({
+              fill: {
+                color: (bE) => {
+                  const colorOpacity = TEMPORAL_COLOR_OPACITY.creation;
+                  return colorOpacity.color;
+                },
+                opacity: (bE) => {
+                  const colorOpacity = TEMPORAL_COLOR_OPACITY.creation;
+                  return colorOpacity.opacity;
+                },
+              },
+            });
+            view.notifyChange();
           });
         }
       );
     });
+
+    levels.forEach((level) => {
+      levelsList.add(level);
+    });
+    console.log(levelsList.containNode(levels.get(2009)));
   }
 }
