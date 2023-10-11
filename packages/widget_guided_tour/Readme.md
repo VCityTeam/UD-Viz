57cfdf330bb1c695cc594f14fdfca90639d75ac4
Lorenzo Marnat <lmarnat14@gmail.com>
2023-10-11 15:23:49 +0200

    feat(GuidedTour): create widget package

---
+++ packages/widget_guided_tour/Readme.md
@@ -0,0 +1,3 @@
+# @ud-viz/widget_guided_tour
+
+[![NPM package version](https://badgen.net/npm/v/@ud-viz/widget_guided_tour)](https://npmjs.com/package/@ud-viz/widget_guided_tour)
---
+++ packages/widget_guided_tour/test/test_1.js
@@ -0,0 +1,19 @@
+() => {
+  return new Promise((resolve) => {
+    /**
+     * @type {typeof import("../../../bin/indexExamples") }
+     */
+    const udviz = window.udviz;
+
+    const crs = 'EPSG:3857';
+
+    const instance = new udviz.widgetGuidedTour.GuidedTour(
+      new udviz.itowns.View(crs, document.createElement('div')),
+      null
+    );
+
+    console.log(instance);
+
+    resolve();
+  });
+};
