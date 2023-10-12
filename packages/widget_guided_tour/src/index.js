06582da72cd3b5d4520679df7ee21ae123e83476
Lorenzo Marnat <lmarnat14@gmail.com>
2023-10-12 11:57:05 +0200

    feat(GuidedTour): creates widget HTML

--- packages/widget_guided_tour/src/index.js
+++ packages/widget_guided_tour/src/index.js
@@ -1,6 +1,16 @@
 import * as itowns from 'itowns';
 
 /**
+ * @typedef {object} Step
+ * @property {number} previous - Index of the previous step. If this is the first step, it should be the index of the step
+ * @property {number} next - Index of the next step. If this is the last step, it should be the index of the step
+ * @property {Array<string>} layers - IDs of the layers to display
+ * @property {Array<string>} media - IDs of the media to display
+ * @property {object} position - Camera position (vec3 with x,y,z coordinates)
+ * @property {object} rotation - Camera rotation (quaternion with x,y,z,w coordinates)
+ */
+
+/**
  * @example
  * Config Example
  * {
@@ -38,13 +48,7 @@ export class GuidedTour {
    * @param {string} configGuidedTour.description - Description of the GuidedTour
    * @param {number} configGuidedTour.startIndex - Index of the first step of the tour
    * @param {number} configGuidedTour.endIndex - Index of the last step of the tour
-   * @param {Array<object>} configGuidedTour.steps - Array of steps
-   * @param {number} configGuidedTour.steps[].previous - Index of the previous step. If this is the first step, it should be the index of the step
-   * @param {number} configGuidedTour.steps[].next - Index of the next step. If this is the last step, it should be the index of the step
-   * @param {Array<string>} configGuidedTour.steps[].layers - IDs of the layers to display
-   * @param {Array<string>} configGuidedTour.steps[].media - IDs of the media to display
-   * @param {object} configGuidedTour.steps[].position - Camera position (vec3 with x,y,z coordinates)
-   * @param {object} configGuidedTour.steps[].rotation - Camera rotation (quaternion with x,y,z,w coordinates)
+   * @param {Array<Step>} configGuidedTour.steps - Array of steps
    */
   constructor(itownsView, configGuidedTour) {
     /** @type {import('itowns').PlanarView} */
@@ -91,17 +95,62 @@ export class GuidedTour {
      *
       @type {HTMLElement} */
     this.domElement = null;
+    this.initHtml();
   }
 
-  addListeners() {}
+  /**
+   * Creates the HTML of the GuidedTour
+   */
+  initHtml() {
+    this.domElement = document.createElement('div');
+    const mediaContainer = document.createElement('div');
+    this.domElement.appendChild(mediaContainer);
 
-  removeListeners() {}
+    const previousButton = document.createElement('button');
+    previousButton.addEventListener(
+      'click',
+      function () {
+        const previousIndex = this.getCurrentStep().previous;
+        this.goToStep(previousIndex);
+      }.bind(this)
+    );
+    this.domElement.appendChild(previousButton);
 
+    const nextButton = document.createElement('button');
+    nextButton.addEventListener(
+      'click',
+      function () {
+        const nextIndex = this.getCurrentStep().next;
+        this.goToStep(nextIndex);
+      }.bind(this)
+    );
+    this.domElement.appendChild(nextButton);
+  }
+
+  /**
+   * Go to the step corresponding to the index
+   *
+   * @param {number} index Index of the step
+   */
+  goToStep(index) {
+    this.currentIndex = index;
+    console.log(this.currentIndex);
+  }
+
+  /**
+   * Dispose the DOM element
+   */
   dispose() {
     this.domElement.remove();
-
     this.itownsView.notifyChange();
+  }
 
-    this.removeListeners();
+  /**
+   * Returns the current step of the tour
+   *
+   * @returns {Step} The current step of the tour
+   */
+  getCurrentStep() {
+    return this.steps[this.currentIndex];
   }
 }
