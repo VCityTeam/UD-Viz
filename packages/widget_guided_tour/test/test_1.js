fb34e6bdf4188831e69bd4e6fbff62e99e6b5928
Lorenzo Marnat <lmarnat14@gmail.com>
2023-10-18 16:29:19 +0200

    test(GuidedTour): instantiate with sample configs

--- packages/widget_guided_tour/test/test_1.js
+++ packages/widget_guided_tour/test/test_1.js
@@ -9,8 +9,35 @@
 
     const instance = new udviz.widgetGuidedTour.GuidedTour(
       new udviz.itowns.View(crs, document.createElement('div')),
-      null,
-      null
+      {
+        steps: [
+          {
+            previous: 0,
+            text: 1,
+            layers: ['layer_1', 'layer_2'],
+            media: [],
+            position: { x: 10, y: 20, z: 30 },
+            rotation: { x: 0.5, y: 0, z: 0.24, w: 0 },
+          },
+          {
+            previous: 0,
+            next: 1,
+            layers: ['layer_3'],
+            media: ['media_1'],
+          },
+        ],
+        name: 'Example',
+        description: 'This is an example of GuidedTour config',
+        startIndex: 0,
+        endIndex: 1,
+      },
+      [
+        {
+          id: 'media_1',
+          type: 'text',
+          value: 'This is an example',
+        },
+      ]
     );
 
     console.log(instance);
