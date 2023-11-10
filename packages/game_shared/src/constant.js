d22ecc3ef7cc4a850a43e2e175a9061732346577
valentinMachado <valentin.machado.cpe@gmail.com>
2023-11-10 14:47:48 +0100

    remove userdata update

--- packages/game_shared/src/constant.js
+++ packages/game_shared/src/constant.js
@@ -4,7 +4,6 @@ module.exports = {
       // server => client
       NEW_GAME: 'new_game',
       GAME_DIFF: 'game_diff',
-      USER_DATA_UPDATE: 'user_data_update',
       // client => server
       READY_FOR_GAME: 'ready_for_game',
       COMMANDS: 'commands',
