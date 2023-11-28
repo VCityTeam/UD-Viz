5d6a0c35170411d2f4827953bfa11c4dff468313
valentinMachado <valentin.machado.cpe@gmail.com>
2023-11-28 16:53:41 +0100

    doc(examples) game_editor example doc + addMarkdown.js to add in a generic way .md

---
+++ examples/assets/js/addMarkdown.js
@@ -0,0 +1,52 @@
+/**
+ * /!\ showdown needs to be imported
+ *
+ * @param {Array<string>} paths - markdown paths
+ */
+const addMarkdown = (paths) => {
+  const loadMarkdown = () => {
+    const mdContainer = document.createElement('div');
+    mdContainer.classList.add('md_container');
+    mdContainer.hidden = true;
+    document.body.appendChild(mdContainer);
+
+    paths.forEach((path) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', path);
+      xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status >= 200 && xhr.status < 300) {
+          const mdHtml = document.createElement('div');
+          mdHtml.innerHTML = new showdown.Converter()
+            .makeHtml(xhr.responseText)
+            .replace(/..\/img\//g, './assets/img/');
+
+          mdContainer.appendChild(mdHtml);
+        }
+      };
+      xhr.send();
+    });
+
+    const showMdContainerButton = document.createElement('button');
+    showMdContainerButton.innerText = '?';
+    showMdContainerButton.classList.add('show_md_container_button');
+    document.body.appendChild(showMdContainerButton);
+
+    showMdContainerButton.onclick = () => {
+      mdContainer.hidden = !mdContainer.hidden;
+      if (mdContainer.hidden) {
+        showMdContainerButton.innerText = '?';
+      } else {
+        showMdContainerButton.innerText = 'X';
+      }
+    };
+  };
+
+  if (!window.showdown) {
+    const scriptTagShowdown = document.createElement('script');
+    scriptTagShowdown.src = './assets/js/libs/showdown.min.js';
+    document.body.appendChild(scriptTagShowdown);
+    scriptTagShowdown.onload = loadMarkdown;
+  } else {
+    loadMarkdown;
+  }
+};
