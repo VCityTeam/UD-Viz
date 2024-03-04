d7e8f8b5d7adf2121189e4b245bc3ace5d803bb0
Mathieu Livebardon <mathieu.livebardon@universite-lyon.fr>
2024-03-04 14:02:57 +0100

    doc(game_node_template):readm + comments in code

--- packages/game_node_template/Readme.md
+++ packages/game_node_template/Readme.md
@@ -2,8 +2,39 @@
 
 [![NPM package version](https://badgen.net/npm/v/@ud-viz/game_node_template)](https://npmjs.com/package/@ud-viz/game_node_template)
 
-Collection of script base on [@ud-viz/game_node](../game_node/Readme.md).
 
+## Overview
+
+`@ud-viz/game_node_template` provides a collection of script base on [@ud-viz/game_node](../game_node/Readme.md) designed to facilitate various aspects of application development, including map management, note handling, and dynamic 3D object associated to DOM Element.
+
+
+## Installation
+
+You can install `@ud-viz/game_node_template` via npm:
+
+```bash
+npm install @ud-viz/game_node_template
+```
+
+## Usage 
+
+### Example
 > you can find an implementation [here](https://github.com/VCityTeam/UD-Viz/blob/master/examples/game_note.html)
 
-> [Documentation](https://vcityteam.github.io/UD-Viz/html/game_node_template/)
+
+
+## Documentation
+
+> [Online Documentation](https://vcityteam.github.io/UD-Viz/html/game_node_template/)
+
+## Contributing
+
+Contributions are welcome! Feel free to submit bug reports, feature requests, or pull requests on the GitHub repository. See [Contributing.md](https://github.com/VCityTeam/UD-Viz/blob/master/docs/static/Contributing.md).
+
+## License
+
+This package is licensed under the [GNU Lesser General Public License Version 2.1](https://github.com/VCityTeam/UD-Viz/blob/master/LICENSE.md), with copyright attributed to the University of Lyon.
+
+## Credits
+
+`@ud-viz/game_node_template` is developed and maintained by [VCityTeam](https://github.com/VCityTeam). See [Contributors.md](https://github.com/VCityTeam/UD-Viz/blob/master/docs/static/Contributors.md).
--- packages/game_node_template/src/DomElement3DCubeManager.js
+++ packages/game_node_template/src/DomElement3DCubeManager.js
@@ -2,11 +2,21 @@ const { ScriptBase, Object3D } = require('@ud-viz/game_shared');
 const { constant } = require('@ud-viz/game_shared_template');
 const { thread } = require('@ud-viz/game_node');
 
+/**
+ * @class Manages the creation and removal of 3D cubes associated with DOM elements.
+ * @augments ScriptBase
+ */
 module.exports = class DomElement3DCubeManager extends ScriptBase {
+  /**
+   * Creates and manages 3D cubes associated with DOM elements based on socket events.
+   */
   init() {
+    // Map to store Object3D instances associated with socket IDs
     this.socketObjects3D = new Map();
 
+    // Event listener for when a new socket wrapper is created
     this.context.on(thread.MESSAGE_EVENT.ON_NEW_SOCKET_WRAPPER, (socketID) => {
+      // Create a new Object3D for the cube
       const newAvatarJitsi = new Object3D({
         static: false,
         components: {
@@ -23,21 +33,33 @@ module.exports = class DomElement3DCubeManager extends ScriptBase {
           },
         },
       });
+      // Set scale of the cube
       newAvatarJitsi.scale.set(100, 100, 100);
+      // Add the cube to the context
       this.context.addObject3D(newAvatarJitsi);
+      // Store the cube object with the corresponding socket ID in the map
       this.socketObjects3D.set(socketID, newAvatarJitsi);
     });
 
+    // Event listener for when a socket wrapper is removed
     this.context.on(
       thread.MESSAGE_EVENT.ON_SOCKET_WRAPPER_REMOVE,
       (socketID) => {
+        // Get the cube associated with the socket ID
         const avatarToRemove = this.socketObjects3D.get(socketID);
+        // Remove the cube from the context
         this.context.removeObject3D(avatarToRemove.uuid);
+        // Remove the cube object from the map
         this.socketObjects3D.delete(socketID);
       }
     );
   }
 
+  /**
+   * Static method to get the ID of the script.
+   *
+   * @returns {string} The ID of the DomElement3DCubeManager script.
+   */
   static get ID_SCRIPT() {
     return 'dom_element_3d_cube_manager_id';
   }
