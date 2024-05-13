a5c879af75f4c40657391dcfc5814a58a4388522
Mathieu Livebardon <mathieu.livebardon@universite-lyon.fr>
2024-05-13 15:55:04 +0200

    feat(e3dtt):parabola STS getspacedpoints

--- packages/extensions_3d_tiles_temporal/src/STSParabola.js
+++ packages/extensions_3d_tiles_temporal/src/STSParabola.js
@@ -6,8 +6,8 @@ export class STSParabola extends STShape {
   constructor(stLayer, options = {}) {
     super(stLayer);
 
-    this.distAxisX = isNaN(options.distAxisX) ? 300 : options.distAxisX; // distance from control point on x axis
-    this.distAxisY = isNaN(options.distAxisY) ? 300 : options.distAxisY;
+    this.distAxisX = isNaN(options.distAxisX) ? 1000 : options.distAxisX; // distance from control point on x axis
+    this.distAxisY = isNaN(options.distAxisY) ? 1000 : options.distAxisY;
     this.height = isNaN(options.height) ? 550 : options.height;
 
     /** @type {Map<string,object>} */
@@ -23,9 +23,10 @@ export class STSParabola extends STShape {
     const view = this.stLayer.view;
     const rootObject3D = this.stLayer.rootObject3D;
     rootObject3D.position.z += this.height;
+    const startPoint = new THREE.Vector2(-this.distAxisX, this.distAxisY);
 
     const path = new THREE.Path();
-    path.moveTo(-this.distAxisX, this.distAxisY);
+    path.moveTo(startPoint.x, startPoint.y);
     const controlPoint1 = new THREE.Vector2(-this.distAxisX / 2, 0);
     path.quadraticCurveTo(controlPoint1.x, controlPoint1.y, 0, 0);
     const controlPoint2 = new THREE.Vector2(this.distAxisX / 2, 0);
@@ -36,7 +37,28 @@ export class STSParabola extends STShape {
       this.distAxisY
     );
 
-    const points = path.getPoints();
+    const numberOfDivisions = 50;
+    let points = path.getSpacedPoints(numberOfDivisions);
+
+    points.forEach((p) => {
+      const geometry = new THREE.SphereGeometry(10, 10, 10);
+      const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+      const sphere = new THREE.Mesh(geometry, material);
+      sphere.position.copy(new THREE.Vector3(p.x, p.y, 0));
+      rootObject3D.add(sphere);
+    });
+
+    const spaceIndexBetweenTwoVersions = Math.round(
+      25 / (this.stLayer.versions.length - 1)
+    );
+
+    const nLeft = 5;
+    const nRight = this.stLayer.versions.length - 1 - nLeft;
+
+    points = points.slice(
+      25 - spaceIndexBetweenTwoVersions * nLeft,
+      25 + spaceIndexBetweenTwoVersions * nRight + 1
+    );
 
     const geometryDisplayed = new THREE.BufferGeometry().setFromPoints(points);
 
