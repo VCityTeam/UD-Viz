0b75cec43854157bd4e4554448580a6935e3faa0
Mathieu Livebardon <mathieu.livebardon@universite-lyon.fr>
2024-05-14 11:08:42 +0200

    feat(e3dtt):sequential display mode on sts vector

--- packages/extensions_3d_tiles_temporal/src/STLayer.js
+++ packages/extensions_3d_tiles_temporal/src/STLayer.js
@@ -9,8 +9,13 @@ export class STLayer {
   constructor(view, rootObject3D, versions) {
     this.view = view;
     this.rootObject3D = rootObject3D;
-
     /** @type {Array<{c3DTLayer: C3DTilesLayer, date: number}>} */
     this.versions = versions;
+    this.versions.sort((v1, v2) => {
+      return v1.date > v2.date;
+    });
+
+    this.dateInterval =
+      this.versions[this.versions.length - 1].date - this.versions[0].date;
   }
 }
